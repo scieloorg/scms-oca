5ba64d8b244d788b4554ee7e5a9fda9f14a0f5ac
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-06-25 19:41:16 -0300

    Cria modelo ScholarlyArticles

---
+++ scholarly_articles/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+from wagtail.admin.edit_handlers import FieldPanel
+
+from . import choices
+
+
+class ScholarlyArticles(models.Model):
+    doi = models.CharField("DOI", max_length=255)
+    doi_url = models.URLField("DOI URL", max_length=255)
+    genre = models.CharField("Resource Type", max_length=255, choices=choices.TYPE_OF_RESOURCE)
+    is_oa = models.BooleanField("Opens Access", max_length=255)
+    journal_is_in_doaj = models.BooleanField("DOAJ", max_length=255)
+    journal_issns = models.CharField("ISSN's", max_length=255)
+    journal_issn_l = models.CharField("ISSN-L", max_length=255)
+    journal_name = models.CharField("Journal Name", max_length=255)
+    published_date = models.DateTimeField("Published Date", max_length=255)
+    publisher = models.CharField("Publisher", max_length=255)
+    title = models.CharField("Title", max_length=255)
+
+    panels = [
+        FieldPanel('doi'),
+        FieldPanel('doi_url'),
+        FieldPanel('genre'),
+        FieldPanel('is_oa'),
+        FieldPanel('journal_is_in_doaj'),
+        FieldPanel('journal_issns'),
+        FieldPanel('journal_issn_l'),
+        FieldPanel('journal_name'),
+        FieldPanel('published_date'),
+        FieldPanel('publisher'),
+        FieldPanel('title'),
+    ]
+
+
