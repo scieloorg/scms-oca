2efabe38f3fe3a2fce4783297c515c0ca1ff3fda
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-07-04 10:00:31 -0300

    Adiciona parâmetros sobre a obrigatoriedade do campo

--- scholarly_articles/models.py
+++ scholarly_articles/models.py
@@ -7,6 +7,17 @@ from . import choices
 
 
 class ScholarlyArticles(models.Model):
+    doi = models.CharField("DOI", max_length=255, null=False, blank=False)
+    doi_url = models.URLField("DOI URL", max_length=255, null=True, blank=True)
+    genre = models.CharField("Resource Type", max_length=255, choices=choices.TYPE_OF_RESOURCE, null=False, blank=False)
+    is_oa = models.BooleanField("Opens Access", max_length=255, null=True, blank=True)
+    journal_is_in_doaj = models.BooleanField("DOAJ", max_length=255, null=True, blank=True)
+    journal_issns = models.CharField("ISSN's", max_length=255, null=False, blank=False)
+    journal_issn_l = models.CharField("ISSN-L", max_length=255, null=False, blank=False)
+    journal_name = models.CharField("Journal Name", max_length=255, null=True, blank=True)
+    published_date = models.DateTimeField("Published Date", max_length=255, null=True, blank=True)
+    publisher = models.CharField("Publisher", max_length=255, null=True, blank=True)
+    title = models.CharField("Title", max_length=255, null=True, blank=True)
 
     panels = [
         FieldPanel('doi'),
