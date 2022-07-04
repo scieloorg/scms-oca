c4ca9dd969234ab388e0e096f24b0dfcbde68c79
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-07-04 09:59:09 -0300

    Remove os campos que serão modificados

--- scholarly_articles/models.py
+++ scholarly_articles/models.py
@@ -7,17 +7,6 @@ from . import choices
 
 
 class ScholarlyArticles(models.Model):
-    doi = models.CharField("DOI", max_length=255)
-    doi_url = models.URLField("DOI URL", max_length=255)
-    genre = models.CharField("Resource Type", max_length=255, choices=choices.TYPE_OF_RESOURCE)
-    is_oa = models.BooleanField("Opens Access", max_length=255)
-    journal_is_in_doaj = models.BooleanField("DOAJ", max_length=255)
-    journal_issns = models.CharField("ISSN's", max_length=255)
-    journal_issn_l = models.CharField("ISSN-L", max_length=255)
-    journal_name = models.CharField("Journal Name", max_length=255)
-    published_date = models.DateTimeField("Published Date", max_length=255)
-    publisher = models.CharField("Publisher", max_length=255)
-    title = models.CharField("Title", max_length=255)
 
     panels = [
         FieldPanel('doi'),
