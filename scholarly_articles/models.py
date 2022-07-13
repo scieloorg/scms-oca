8adcd63f8793c14476dd2036792e806a3bfffcbd
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-07-13 17:17:22 -0300

    Adiciona Affiliations

--- scholarly_articles/models.py
+++ scholarly_articles/models.py
@@ -72,3 +72,23 @@ class Contributors(models.Model):
         FieldPanel('authenticated_orcid'),
         FieldPanel('affiliation'),
     ]
+
+
+class Affiliations(models.Model):
+    institution_name = models.CharField(_("Institution Name"), max_length=100, null=False, blank=False)
+    institution_acronym = models.CharField(_("Institution Acronym"), max_length=10, null=True, blank=True)
+    institution_place = models.CharField(_("Institution Place"), max_length=255, null=True, blank=True)
+    institution_department = models.CharField(_("Institution Department"), max_length=255, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.institution_name
+
+    def __str__(self):
+        return self.institution_name
+
+    panels = [
+        FieldPanel('institution_name'),
+        FieldPanel('institution_acronym'),
+        FieldPanel('institution_place'),
+        FieldPanel('institution_department'),
+    ]
