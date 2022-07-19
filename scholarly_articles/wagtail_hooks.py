942480fa05747f3839794f0211dfc9d6ecd9f589
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-07-19 20:02:31 -0300

    Adiciona RawUnpaywallAdmin

--- scholarly_articles/wagtail_hooks.py
+++ scholarly_articles/wagtail_hooks.py
@@ -27,6 +27,28 @@ class ScholarlyArticlesAdmin(ModelAdmin):
     search_fields = ('doi',)
 
 
+class RawUnpaywallAdmin(ModelAdmin):
+    model = RawUnpaywall
+    menu_label = _('RawUnpaywall')  # ditch this to use verbose_name_plural from model
+    menu_icon = 'folder-open-inverse'  # change as required
+    #menu_order = 100  # will put in 3rd place (000 being 1st, 100 2nd)
+    add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
+    exclude_from_explorer = False  # or True to exclude pages of this type from Wagtail's explorer view
+
+    list_display = (
+        'doi',
+        'harvesting_creation',
+        'is_paratext',
+        'year',
+        'resource_type',
+        'update',
+        'json',
+    )
+
+    list_filter = (_('year'),)
+    search_fields = ('doi',)
+
+
         'journal_issn_l',
         'journal_name',
         'oa_status',
