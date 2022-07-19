9c73687d7b519d87c406a4a7a6a7e5eee69f73b7
InnoLabs <gitnnolbas@innolabs.com.br>
2022-07-19 19:51:25 -0300

    Adiciona customs css e js.

--- core/static/js/custom.js
+++ core/static/js/custom.js
@@ -1,1 +1,7 @@
 /* your custom js go here */
+
+$('.lang-select').change(function(){
+    let url = $(this).find(':selected').attr('data-url');
+    $('#language').val($(this).val());
+    $('#form_lang').submit();
+});
