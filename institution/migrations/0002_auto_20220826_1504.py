c1020a6b768c9e3535e59a42df06d1f0bf2ff485
Jamil Atta Junior <atta.jamil@innolabs.com.br>
2022-09-01 11:40:51 -0300

    Remove uma migração com conflito de ID

--- institution/migrations/0002_auto_20220826_1504.py
+++ institution/migrations/0002_auto_20220826_1504.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('institution', '0004_institution_acronym'),
+        ('institution', '0001_initial'),
     ]
 
     operations = [
