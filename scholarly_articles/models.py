48566912482cebfebd02cf2464e8dbad7352ba07
Jamil Atta Junior <atta.jamil@innolabs.com.br>
2022-09-15 10:39:50 -0300

    Remove um param desnecessário em contributors

--- scholarly_articles/models.py
+++ scholarly_articles/models.py
@@ -19,7 +19,7 @@ class ScholarlyArticles(models.Model):
                                      blank=True)
     use_license = models.CharField(_("Use License"), max_length=25, choices=choices.LICENSE, null=True, blank=True)
     apc = models.CharField(_("Article Processing Charge"), max_length=25, choices=choices.APC, null=True, blank=True)
-    contributors = models.ManyToManyField("Contributors", verbose_name=_("Contributors"), null=True, blank=True)
+    contributors = models.ManyToManyField("Contributors", verbose_name=_("Contributors"), blank=True)
     journal = models.ForeignKey("Journals", verbose_name=_("Journals"), on_delete=models.SET_NULL, max_length=255, null=True, blank=True)
     source = models.CharField(_("Record Source"), max_length=255, choices=choices.SOURCE, null=True, blank=True)
 
