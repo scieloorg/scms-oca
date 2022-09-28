f5a30b7d558f8dd01262dbd5a3366d0f29a21b7d
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-09-28 15:53:57 -0300

    Adiciona create_indicator.py

--- indicator/scripts/create_indicator.py
+++ indicator/scripts/create_indicator.py
@@ -1,9 +1,21 @@
-from scholarly_articles.tasks import load_journal_articles
-
+from indicator import controller
+from institution.models import Institution
 
 def run():
-    """
-    Load the article to ScholarlyArticles.
-    """
-    load_journal_articles.apply_async(kwargs={"from_year": 1900,
-                                              "resource_type": "journal-article"})
+    for institution in Institution.objects.all():
+
+    indicator = controller.generate_indicator(
+        institution="Universidade de São Paulo",
+        practice="literatura em acesso aberto",
+        action=None,
+        classification=None,
+        thematic_area=None,
+        start_date=None,
+        end_date=None,
+        location=None,
+        return_data=True,
+        return_rows=True,
+    )
+
+    indicator.creator_id = 1
+    indicator.save()
