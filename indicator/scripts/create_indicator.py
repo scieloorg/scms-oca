681a0dc8f11bc0c05cf716b3bcf864d4737bdbf2
Roberta Takenaka <roberta.takenaka@scielo.org>
2022-09-29 01:45:02 -0300

    Cria indicador de ações no contexto institucional

--- indicator/scripts/create_indicator.py
+++ indicator/scripts/create_indicator.py
@@ -1,21 +1,34 @@
 from indicator import controller
 from institution.models import Institution
+from usefulmodels.models import Action
+
+# def run():
+#     for institution in Institution.objects.all():
+
+#     indicator = controller.generate_indicator(
+#         institution="Universidade de São Paulo",
+#         practice="literatura em acesso aberto",
+#         action=None,
+#         classification=None,
+#         thematic_area=None,
+#         start_date=None,
+#         end_date=None,
+#         location=None,
+#         return_data=True,
+#         return_rows=True,
+#     )
+
+#     indicator.creator_id = 1
+#     indicator.save()
+
+
+def generate_institutions_indicators():
+    creator_id = 1
+    for action in Action.objects.all():
+        title = f"Número de {action} [QUALIFICATION] no contexto institutional"
+        controller.generate_indicator_in_institutional_context(
+            title, action, creator_id)
+
 
 def run():
-    for institution in Institution.objects.all():
-
-    indicator = controller.generate_indicator(
-        institution="Universidade de São Paulo",
-        practice="literatura em acesso aberto",
-        action=None,
-        classification=None,
-        thematic_area=None,
-        start_date=None,
-        end_date=None,
-        location=None,
-        return_data=True,
-        return_rows=True,
-    )
-
-    indicator.creator_id = 1
-    indicator.save()
+    generate_institutions_indicators()
