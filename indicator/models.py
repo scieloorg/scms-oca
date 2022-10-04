464a8b930b8b9ce08764b69997b948abaf9f9364
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-10-04 13:30:31 -0300

    Remove campos comuns em 'indicator'

--- indicator/models.py
+++ indicator/models.py
@@ -1,40 +1,24 @@
 from django.db import models
 from django.utils.translation import gettext as _
-from taggit.managers import TaggableManager
 from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
 
 from core.models import CommonControlField
+from core_oca.models import CommonFields
 
 from . import choices
 from .forms import IndicatorDirectoryForm
-from usefulmodels.models import Action, Practice, ThematicArea
-from institution.models import Institution
+
 from location.models import Location
 
-class Indicator(CommonControlField):
-    title = models.CharField(_("Title"), max_length=255, null=False, blank=False)
-    description = models.TextField(_("Description"), max_length=1000,
-                                   null=True, blank=True)
-    versioning = models.ForeignKey("Versioning", verbose_name=_("Versioning"), on_delete=models.SET_NULL,
-                                   max_length=255, null=True, blank=True)
-    action = models.ForeignKey(Action, verbose_name=_("Action"), null=True, blank=True, on_delete=models.SET_NULL)
+
+class Indicator(CommonFields, CommonControlField):
     classification = models.CharField(_("Classification"), choices=choices.classification,
                                       max_length=255, null=True, blank=True)
-    practice = models.ForeignKey(Practice, verbose_name=_("Practice"),
-                                 null=True, blank=True, on_delete=models.SET_NULL)
-    thematic_areas = models.ManyToManyField(ThematicArea, verbose_name=_("Thematic Area"), blank=True)
-    institutions = models.ManyToManyField(Institution, verbose_name=_("Institution"), blank=True)
+    versioning = models.ForeignKey("Versioning", verbose_name=_("Versioning"), on_delete=models.SET_NULL,
+                                   max_length=255, null=True, blank=True)
     locations = models.ManyToManyField(Location, verbose_name=_("Location"),  blank=True)
-    start_date = models.DateField(_("Start Date"), max_length=255, null=True, blank=True)
-    end_date = models.DateField(_("End Date"), max_length=255, null=True, blank=True)
-    link = models.URLField(_("Link"), null=True, blank=True)
     file_csv = models.FileField(_("CSV File"), null=True, blank=True)
     file_json = models.JSONField(_("JSON File"), null=True, blank=True)
-    keywords = TaggableManager(_("Keywords"), blank=True)
-    record_status = models.CharField(_("Record status"), choices=choices.status,
-                                     max_length=255, null=True, blank=True)
-    source = models.CharField(_("Source"), max_length=255, null=True, blank=True)
-
 
     class Meta:
         indexes = [
