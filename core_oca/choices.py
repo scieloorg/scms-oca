83f2565b1dd2ec3828511e540b91b3316340b694
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-10-04 13:36:06 -0300

    Adiciona 'status' em 'choice.py' em 'core_oca'

---
+++ core_oca/choices.py
@@ -0,0 +1,8 @@
+from django.utils.translation import gettext as _
+
+status = (
+    ('', ''),
+    ('WIP', _('WORK IN PROGRESS')),
+    ('TO MODERATE', _('TO MODERATE')),
+    ('PUBLISHED', _('PUBLISHED')),
+)
