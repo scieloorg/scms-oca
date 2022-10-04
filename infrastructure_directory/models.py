6c6fce495ba31204dc5fa53c28b8b31e8554454d
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-10-04 13:31:58 -0300

    Remove campos comuns em 'infrastructure_directory'

--- infrastructure_directory/models.py
+++ infrastructure_directory/models.py
@@ -2,44 +2,23 @@ import os
 
 from django.db import models
 from django.utils.translation import gettext as _
-from taggit.managers import TaggableManager
 from wagtail.admin.edit_handlers import FieldPanel, HelpPanel
 from wagtail.documents.edit_handlers import DocumentChooserPanel
 
 from core.models import CommonControlField
-from institution.models import Institution
-from usefulmodels.models import ThematicArea, Practice, Action
+from core_oca.models import CommonFields
 
 from . import choices
 from .forms import InfrastructureDirectoryFileForm, InfrastructureDirectoryForm
 
 
-class InfrastructureDirectory(CommonControlField):
+class InfrastructureDirectory(CommonFields, CommonControlField):
     class Meta:
         verbose_name_plural = _('Infraestructure Directory')
-
-    title = models.CharField(_("Title"), max_length=255, null=False, blank=False)
-    link = models.URLField(_("Link"), null=False, blank=False)
-    description = models.TextField(_("Description"), max_length=1000,
-                                   null=True, blank=True)
-
-    institutions = models.ManyToManyField(Institution, verbose_name=_("Institution"), blank=True)
-    thematic_areas = models.ManyToManyField(ThematicArea, verbose_name=_("Thematic Area"), blank=True)
-
-    practice = models.ForeignKey(Practice, verbose_name=_("Practice"),
-                                 null=True, blank=True, on_delete=models.SET_NULL)
-    action = models.ForeignKey(Action, verbose_name=_("Action"), null=True, blank=True, on_delete=models.SET_NULL)
-
 
     classification = models.CharField(_("Classification"), choices=choices.classification,
                                       max_length=255, null=True, blank=True)
 
-    keywords = TaggableManager(_("Keywords"), blank=True)
-
-    record_status = models.CharField(_("Record status"), choices=choices.status,
-                                     max_length=255, null=True, blank=True)
-
-    source = models.CharField(_("Source"), max_length=255, null=True, blank=True)
 
     panels = [
         HelpPanel('Portais, plataformas, servidores, repositórios e serviços brasileiros que operam em acesso aberto objetos de comunicação de comunicação de pesquisas, recursos de apoio e resultantes de pesquisas e em acesso aberto.'),
