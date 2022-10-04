f9ffcd4a4cdca8a87ccd30ce22810f492bc37f85
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-10-04 13:28:45 -0300

    Remove campos comuns em 'event_directory'

--- event_directory/models.py
+++ event_directory/models.py
@@ -2,54 +2,28 @@ import os
 
 from django.db import models
 from django.utils.translation import gettext as _
-from taggit.managers import TaggableManager
 from wagtail.admin.edit_handlers import FieldPanel, HelpPanel
 from wagtail.documents.edit_handlers import DocumentChooserPanel
 
 from core.models import CommonControlField
-from institution.models import Institution
+from core_oca.models import CommonFields
+
 from location.models import Location
-from usefulmodels.models import ThematicArea, Practice, Action
 
 from . import choices
 from .forms import EventDirectoryFileForm, EventDirectoryForm
 
 
-class EventDirectory(CommonControlField):
+class EventDirectory(CommonFields, CommonControlField):
     class Meta:
         verbose_name_plural = _('EventDirectory Directory')
 
-    title = models.CharField(_("Title"), max_length=255, null=False, blank=False)
-    link = models.URLField(_("Link"), null=False, blank=False)
-    description = models.TextField(_("Description"), max_length=1000,
-                                   null=True, blank=True)
-    start_date = models.DateField(_("Start Date"), max_length=255,
-                                  null=True, blank=True)
-    end_date = models.DateField(_("End Date"), max_length=255,
-                                null=True, blank=True)
-    start_time = models.TimeField(_("Start Time"), max_length=255,
-                                  null=True, blank=True)
-    end_time = models.TimeField(_("End Time"), max_length=255,
-                                null=True, blank=True)
-
     locations = models.ManyToManyField(Location, verbose_name=_("Location"),  blank=True)
-    organization = models.ManyToManyField(Institution, verbose_name=_(
-        "Organization"), blank=True, help_text=_('Instituições responsáveis pela organização do evento.'))
-    thematic_areas = models.ManyToManyField(ThematicArea, verbose_name=_("Thematic Area"), blank=True)
-
-    practice = models.ForeignKey(Practice, verbose_name=_("Practice"),
-                                null=True, blank=True, on_delete=models.SET_NULL)
-    action = models.ForeignKey(Action, verbose_name=_("Action"), null=True, blank=True, on_delete=models.SET_NULL)
 
     classification = models.CharField(_("Classification"), choices=choices.classification,
                                       max_length=255, null=True, blank=True)
-    keywords = TaggableManager(_("Keywords"), blank=True)
 
     attendance = models.CharField(_("Attendance"), choices=choices.attendance_type, max_length=255, null=True, blank=True)
-
-    record_status = models.CharField(_("Record status"), choices=choices.status,
-                                     max_length=255, null=True, blank=True)
-    source = models.CharField(_("Source"), max_length=255, null=True, blank=True)
 
     panels = [
         HelpPanel('Encontros, congressos, workshops, seminários realizados no Brasil (presenciais, virtuais ou híbridos) cujo tema principal seja a promoção da Ciência Aberta'),
@@ -58,7 +32,7 @@ class EventDirectory(CommonControlField):
         FieldPanel('source'),
 
         FieldPanel('description'),
-        FieldPanel('organization'),
+        FieldPanel('institutions', heading=_("Organization")),
 
         FieldPanel('start_date'),
         FieldPanel('end_date'),
