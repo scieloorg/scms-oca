1efcb57b99a72696f11abb11c11a890f92991064
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-10-04 13:26:54 -0300

    Remove campos comuns em 'education_directory'

--- education_directory/models.py
+++ education_directory/models.py
@@ -2,56 +2,29 @@ import os
 
 from django.db import models
 from django.utils.translation import gettext as _
-from taggit.managers import TaggableManager
 from wagtail.admin.edit_handlers import FieldPanel, HelpPanel
 from wagtail.documents.edit_handlers import DocumentChooserPanel
 
 from core.models import CommonControlField
-from institution.models import Institution
+from core_oca.models import CommonFields
+
 from location.models import Location
-from usefulmodels.models import Practice, ThematicArea, Action
 
 from .forms import EducationDirectoryFileForm, EducationDirectoryForm
 
 from . import choices
 
-class EducationDirectory(CommonControlField):
+class EducationDirectory(CommonFields, CommonControlField):
     class Meta:
         verbose_name_plural = _('Education Directory')
 
-    title = models.CharField(_("Title"), max_length=255, null=False, blank=False)
-    link = models.URLField(_("Link"), null=False, blank=False)
-    description = models.TextField(_("Description"), max_length=1000,
-                                   null=True, blank=True)
-    start_date = models.DateField(_("Start Date"), max_length=255,
-                                  null=True, blank=True)
-    end_date = models.DateField(_("End Date"), max_length=255,
-                                  null=True, blank=True)
-    start_time = models.TimeField(_("Start Time"), max_length=255,
-                                  null=True, blank=True)
-    end_time = models.TimeField(_("End Time"), max_length=255,
-                                  null=True, blank=True)
-
     locations = models.ManyToManyField(Location, verbose_name=_("Location"),  blank=True)
-    institutions = models.ManyToManyField(Institution, verbose_name=_("Institution"), blank=True)
-    thematic_areas = models.ManyToManyField(ThematicArea, verbose_name=_("Thematic Area"), blank=True)
-
-    practice = models.ForeignKey(Practice, verbose_name=_("Practice"),
-                                null=True, blank=True, on_delete=models.SET_NULL)
-    action = models.ForeignKey(Action, verbose_name=_("Action"), null=True, blank=True, on_delete=models.SET_NULL)
 
     classification = models.CharField(_("Classification"), choices=choices.classification,
                                       max_length=255, null=True, blank=True)
 
-    keywords = TaggableManager(_("Keywords"), blank=True)
-
     attendance = models.CharField(_("Attendance"), choices=choices.attendance_type,
                                   max_length=255, null=True, blank=True)
-
-    record_status = models.CharField(_("Record status"), choices=choices.status,
-                                      max_length=255, null=True, blank=True)
-
-    source = models.CharField(_("Source"), max_length=255, null=True, blank=True)
 
     panels = [
         HelpPanel('Cursos livres, disciplinas de graduação e pós-graduação ministrados por instituições brasileiras – presenciais ou EAD- para promover a adoção dos princípios e práticas de ciência aberta por todos os envolvidos no processo de pesquisa.'),
