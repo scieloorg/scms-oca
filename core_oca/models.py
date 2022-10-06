d6bc4c0ca7adc2271ecbc1938058e31f63beb4fd
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-10-06 09:15:08 -0300

    Corrige 'core_oca'

--- core_oca/models.py
+++ core_oca/models.py
@@ -2,20 +2,17 @@ from django.db import models
 from django.utils.translation import gettext as _
 from taggit.managers import TaggableManager
 
+from core.models import CommonControlField
+
 from institution.models import Institution
 from usefulmodels.models import ThematicArea, Practice, Action
 from . import choices
 
 
-class CommonFields(models.Model):
+class CommonFields(CommonControlField):
     title = models.CharField(_("Title"), max_length=255, null=False, blank=False)
     link = models.URLField(_("Link"), null=True, blank=True)
     description = models.TextField(_("Description"), max_length=1000, null=True, blank=True)
-    start_date = models.DateField(_("Start Date"), max_length=255, null=True, blank=True)
-    end_date = models.DateField(_("End Date"), max_length=255, null=True, blank=True)
-    start_time = models.TimeField(_("Start Time"), max_length=255, null=True, blank=True)
-    end_time = models.TimeField(_("End Time"), max_length=255, null=True, blank=True)
-    institutions = models.ManyToManyField(Institution, verbose_name=_("Institution"), blank=True)
     thematic_areas = models.ManyToManyField(ThematicArea, verbose_name=_("Thematic Area"), blank=True)
     practice = models.ForeignKey(Practice, verbose_name=_("Practice"), null=True, blank=True, on_delete=models.SET_NULL)
     action = models.ForeignKey(Action, verbose_name=_("Action"), null=True, blank=True, on_delete=models.SET_NULL)
