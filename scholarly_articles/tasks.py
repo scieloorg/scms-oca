ee299eaf8ef621cd27412e04bc705f1da34c5971
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-10-09 11:20:43 -0300

    Adiciona 'task'

--- scholarly_articles/tasks.py
+++ scholarly_articles/tasks.py
@@ -3,7 +3,7 @@ import gzip
 from django.contrib.auth import get_user_model
 
 from config import celery_app
-from scholarly_articles.unpaywall import load_data, unpaywall
+from scholarly_articles.unpaywall import load_data, unpaywall, supplementary
 
 User = get_user_model()
 
@@ -41,3 +41,25 @@ def load_journal_articles(user_id, from_year=1900, resource_type='journal-articl
     user = User.objects.get(id=user_id)
 
     load_data.load(from_year, resource_type, user)
+
+
+@celery_app.task()
+def load_supplementary(user_id, file_path):
+    """
+    Load the data from supplementary file.
+
+    Sync or Async function
+
+    Param file_path: String with the path of the CSV file.
+    Param user: The user id passed by kwargs on tasks.kwargs
+    """
+    user = User.objects.get(id=user_id)
+
+    try:
+        with gzip.open(file_path, "rb") as f:
+            for line, row in enumerate(f):
+                supplementary.load(line, row, user)
+    except OSError:
+        with open(file_path, "rb") as f:
+            for line, row in enumerate(f):
+                supplementary.load(line, row, user)
