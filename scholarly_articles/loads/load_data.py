a039dc0045ca54523f1d5de2617ee3791e35ae5f
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-10-18 15:01:41 -0300

    Adiciona 'load_data.py'

---
+++ scholarly_articles/loads/load_data.py
@@ -0,0 +1,168 @@
+from django.db.utils import DataError
+
+from scholarly_articles import models
+
+
+class ArticleSaveError(Exception):
+    ...
+
+class JournalSaveError(Exception):
+    ...
+
+class ContributorSaveError(Exception):
+    ...
+
+class AffiliationSaveError(Exception):
+    ...
+
+
+def get_params(row, attribs):
+    params = {}
+    for att in attribs:
+        if row.get(att):
+            params[att] = row.get(att)
+    return params
+
+
+def load_article(row, source):
+    articles = models.ScholarlyArticle.objects.filter(doi=row.get('doi'))
+    try:
+        article = articles[0]
+    except IndexError:
+        article = models.ScholarlyArticle()
+        article.doi = row.get('doi')
+    if(
+        not article.title or
+        not article.volume or
+        not article.open_access_status or
+        not article.apc or
+        not article.journal
+    ):
+        change = True;
+    article.title = article.title or row.get('title')
+    article.volume = article.volume or row.get('volume')
+    article.number = article.number or row.get('number')
+    article.year = article.year or row.get('year')
+    article.open_access_status = article.open_access_status or row.get('oa_status')
+    if len(row.get('oa_locations')) > 0:
+        article.use_license = article.use_license or row.get('oa_locations')[0].get('license')
+    article.apc = article.apc or row.get('apc')
+    article.journal = article.journal or load_journal(row, source)
+    article.source = " | ".join([article.source, source]) \
+        if change and article.source and article.source != source and len(str(article.source).split(' | ')) == 1 \
+        else source
+    try:
+        article.save()
+    except (DataError, TypeError) as e:
+        raise ArticleSaveError(e)
+
+    for author in row.get('z_authors') or []:
+        contributor = get_one_contributor(author, source)
+        article.contributors.add(contributor)
+        article.save()
+
+    return article
+
+
+def load_journal(row, source):
+    attribs = ['journal_issns', 'journal_issn_l', 'journal_name']
+    params = get_params(row, attribs)
+
+    journals = models.Journal.objects.filter(**params)
+    try:
+        journal = journals[0]
+    except IndexError:
+        journal = models.Journal()
+    journal.journal_is_in_doaj = journal.journal_is_in_doaj or row.get('journal_is_in_doaj')
+    journal.journal_issns = journal.journal_issns or row.get('journal_issns')
+    journal.journal_issn_l = journal.journal_issn_l or row.get('journal_issn_l')
+    journal.journal_name = journal.journal_name or row.get('journal_name')
+    journal.publisher = journal.journal_name or row.get('publisher')
+    journal.source = " | ".join([journal.source, source]) \
+        if journal.source and journal.source != source and len(str(journal.source).split(' | ')) == 1 \
+        else source
+    try:
+        journal.save()
+    except (DataError, TypeError) as e:
+        raise JournalSaveError(e)
+
+    return journal
+
+
+def get_one_contributor(author, source):
+    attribs = ['family', 'given']
+    params = get_params(author, attribs)
+    if author.get('ORCID'):
+        params['orcid'] = author.get('ORCID')
+
+    contributors = models.Contributor.objects.filter(**params)
+    try:
+        contributor = contributors[0]
+    except IndexError:
+        contributor = models.Contributor()
+    contributor.family = contributor.family or author.get('family')
+    contributor.given = contributor.given or author.get('given')
+    contributor.orcid = contributor.orcid or author.get('ORCID')
+    contributor.authenticated_orcid = contributor.authenticated_orcid or author.get('authenticated-orcid')
+    contributor.source = " | ".join([contributor.source, source]) \
+        if contributor.source and contributor.source != source and len(str(contributor.source).split(' | ')) == 1 \
+        else source
+    try:
+        contributor.save()
+    except (DataError, TypeError) as e:
+        raise ContributorSaveError(e)
+
+    if author.get('affiliation'):
+        aff = load_affiliation(author.get('affiliation')[0].get('name'), source)
+        contributor.affiliation.add(aff)
+        contributor.save()
+
+    return contributor
+
+
+def load_affiliation(affiliation_name, source):
+    if affiliation_name:
+        try:
+            affiliations = models.Affiliation.objects.filter(name=affiliation_name)
+            affiliation = affiliations[0]
+        except IndexError:
+            affiliation = models.Affiliation()
+            affiliation.name = affiliation_name
+            affiliation.source = source
+        try:
+            affiliation.save()
+        except (DataError, TypeError) as e:
+            raise AffiliationSaveError(e)
+
+        return affiliation
+
+
+def load(from_year, user):
+    """
+    Load all data with a specific resource_type and year from RawUnpaywall model
+    to ScholarlyArticles model.
+
+    Param from_year: Is a interger, example: 2000
+    Param resource_type: Is a string, that represent a type in RawUnpaywall
+    Param user: The user instance
+    """
+
+    # About iterator: https://docs.djangoproject.com/en/3.2/ref/models/querysets/#django.db.models.query.QuerySet.iterator
+    raw_record = models.RawRecord.objects.filter(year__gte=from_year).iterator()
+    # rawunpaywall = models.RawUnpaywall.objects.filter(year__gte=from_year, resource_type=resource_type)
+    for item in raw_record:
+        try:
+            load_article(item.json, item.source)
+        except (ArticleSaveError, JournalSaveError, ContributorSaveError, AffiliationSaveError) as e:
+            try:
+                error = models.ErrorLog()
+                error.error_type = str(type(e))
+                error.error_message = str(e)[:255]
+                error.error_description = "Erro on processing the RawRecord to ScholarlyArticles."
+                error.data_reference = "id:%s" % str(item.id)
+                error.data = item.json
+                error.data_type = "ScholarlyArticles"
+                error.creator = user
+                error.save()
+            except (DataError, TypeError):
+                pass
