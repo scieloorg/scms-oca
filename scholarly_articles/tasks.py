feb9bef8b6e99501fb973fa71fa21fb4d30ca099
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-10-18 14:59:58 -0300

    Ajustes em 'tasks.py'

--- scholarly_articles/tasks.py
+++ scholarly_articles/tasks.py
@@ -3,14 +3,15 @@ import gzip
 from django.contrib.auth import get_user_model
 
 from config import celery_app
-from scholarly_articles.unpaywall import load_data, unpaywall, supplementary
+from scholarly_articles.loads import load_data_raw, load_data
+
 
 User = get_user_model()
 
 @celery_app.task()
-def load_unpaywall(user_id, file_path):
+def load_data_raw_json(user_id, file_path, file_source='Unpaywall'):
     """
-    Load the data from unpaywall file.
+    Load the data from loads file.
 
     Sync or Async function
 
@@ -22,44 +23,23 @@ def load_unpaywall(user_id, file_path):
     try:
         with gzip.open(file_path, "rb") as f:
             for line, row in enumerate(f):
-                unpaywall.load(line, row, user)
+                load_data_raw.load(line, row, user, file_source)
     except OSError:
         with open(file_path, "rb") as f:
             for line, row in enumerate(f):
-                unpaywall.load(line, row, user)
+                load_data_raw.load(line, row, user, file_source)
 
 
 @celery_app.task()
-def load_journal_articles(user_id, from_year=1900, resource_type='journal-article'):
+def load_articles_data(user_id):
     """
-    Load the data from unpaywall model to ScholarlyArticles.
+    Load the data from loads file.
 
     Sync or Async function
 
+    Param file_path: String with the path of the JSON like file compressed or not.
     Param user: The user id passed by kwargs on tasks.kwargs
     """
     user = User.objects.get(id=user_id)
 
-    load_data.load(from_year, resource_type, user)
-
-
-@celery_app.task()
-def load_supplementary(user_id, file_path):
-    """
-    Load the data from supplementary file.
-
-    Sync or Async function
-
-    Param file_path: String with the path of the CSV file.
-    Param user: The user id passed by kwargs on tasks.kwargs
-    """
-    user = User.objects.get(id=user_id)
-
-    try:
-        with gzip.open(file_path, "rb") as f:
-            for line, row in enumerate(f):
-                supplementary.load(line, row, user)
-    except OSError:
-        with open(file_path, "rb") as f:
-            for line, row in enumerate(f):
-                supplementary.load(line, row, user)
+    load_data.load(from_year=0, user=user)
