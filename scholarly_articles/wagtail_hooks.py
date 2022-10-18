27e1d9bd6a49172e401452695bc9471c6aa4806e
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-10-18 14:58:26 -0300

    Ajustes em 'wagtail_hooks.py'

--- scholarly_articles/wagtail_hooks.py
+++ scholarly_articles/wagtail_hooks.py
@@ -2,16 +2,17 @@ from django.utils.translation import gettext as _
 
 from wagtail.contrib.modeladmin.options import (ModelAdmin, modeladmin_register, ModelAdminGroup)
 
-from .models import (ScholarlyArticles, Contributors, Affiliations, Journals, RawUnpaywall, SupplementaryData, ErrorLog)
+from .models import (ScholarlyArticle, Contributor, Affiliation, Journal, RawRecord, ErrorLog)
 
 
-class ScholarlyArticlesAdmin(ModelAdmin):
-    model = ScholarlyArticles
+class ScholarlyArticleAdmin(ModelAdmin):
+    model = ScholarlyArticle
     menu_label = _('Scholarly Articles')  # ditch this to use verbose_name_plural from model
     menu_icon = 'folder-open-inverse'  # change as required
     #menu_order = 100  # will put in 3rd place (000 being 1st, 100 2nd)
     add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
     exclude_from_explorer = False  # or True to exclude pages of this type from Wagtail's explorer view
+    inspect_view_enabled = True
 
     def all_contributors(self, obj):
         return " | ".join([str(c) for c in obj.contributors.all()])
@@ -19,12 +20,7 @@ class ScholarlyArticlesAdmin(ModelAdmin):
     list_display = (
         'doi',
         'title',
-        'volume',
-        'number',
         'year',
-        'open_access_status',
-        'use_license',
-        'apc',
         'all_contributors',
         'journal',
         'source',
@@ -34,30 +30,29 @@ class ScholarlyArticlesAdmin(ModelAdmin):
     search_fields = ('doi',)
 
 
-class RawUnpaywallAdmin(ModelAdmin):
-    model = RawUnpaywall
-    menu_label = _('RawUnpaywall')  # ditch this to use verbose_name_plural from model
+class RawRecordAdmin(ModelAdmin):
+    model = RawRecord
+    menu_label = _('Raw Record')  # ditch this to use verbose_name_plural from model
     menu_icon = 'folder-open-inverse'  # change as required
     #menu_order = 100  # will put in 3rd place (000 being 1st, 100 2nd)
     add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
     exclude_from_explorer = False  # or True to exclude pages of this type from Wagtail's explorer view
+    inspect_view_enabled = True
 
     list_display = (
         'doi',
         'harvesting_creation',
-        'is_paratext',
         'year',
         'resource_type',
-        'update',
-        'json',
+        'source',
     )
 
     list_filter = ('year',)
     search_fields = ('doi',)
 
 
-class JournalsAdmin(ModelAdmin):
-    model = Journals
+class JournalAdmin(ModelAdmin):
+    model = Journal
     menu_label = _('Journals')  # ditch this to use verbose_name_plural from model
     menu_icon = 'folder-open-inverse'  # change as required
     #menu_order = 000  # will put in 3rd place (000 being 1st, 100 2nd)
@@ -70,34 +65,39 @@ class JournalsAdmin(ModelAdmin):
         'journal_name',
         'publisher',
         'journal_is_in_doaj',
+        'source',
     )
 
     #list_filter = (_('journal_issn_l'),)
     search_fields = (_('journal_issn_l)'),)
 
 
-class ContributorsAdmin(ModelAdmin):
-    model = Contributors
+class ContributorAdmin(ModelAdmin):
+    model = Contributor
     menu_label = _('Contributors')
     menu_icon = 'folder-open-inverse'
     #menu_order = 200
     add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
     exclude_from_explorer = False  # or True to exclude pages of this type from Wagtail's explorer view
 
+    def all_affiliations(self, obj):
+        return " | ".join([str(c) for c in obj.affiliation.all()])
+
     list_display = (
         'family',
         'given',
         'orcid',
         'authenticated_orcid',
-        'affiliation',
+        'all_affiliations',
+        'source',
     )
 
     # list_filter = ('orcid',)
     search_fields = ('orcid', 'family', 'given', 'affiliation',)
 
 
-class AffiliationsAdmin(ModelAdmin):
-    model = Affiliations
+class AffiliationAdmin(ModelAdmin):
+    model = Affiliation
     menu_label = _('Affiliations')
     menu_icon = 'folder-open-inverse'
     add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
@@ -105,24 +105,10 @@ class AffiliationsAdmin(ModelAdmin):
     list_display = (
         'name',
         'official',
+        'source',
     )
     #list_filter = ('name',)
     search_fields = ('name', 'official',)
-
-
-class SupplementaryDataAdmin(ModelAdmin):
-    model = SupplementaryData
-    menu_label = _('Supplementary Data')
-    menu_icon = 'folder-open-inverse'
-    inspect_view_enabled = True
-    add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
-    exclude_from_explorer = False  # or True to exclude pages of this type from Wagtail's explorer view
-    list_display = (
-        'doi',
-        'year',
-    )
-    #list_filter = ('name',)
-    search_fields = ('doi', )
 
 
 class ErrorLogAdmin(ModelAdmin):
@@ -140,17 +126,16 @@ class ErrorLogAdmin(ModelAdmin):
     search_fields = ('document_id',)
 
 
-class ScholarlyArticlesAdminGroup(ModelAdminGroup):
+class ScholarlyArticleAdminGroup(ModelAdminGroup):
     menu_label = _('Articles Directory')
     menu_icon = 'folder-open-inverse'  # change as required
     menu_order = 200  # will put in 3rd place (000 being 1st, 100 2nd)
-    items = (JournalsAdmin,
-             ScholarlyArticlesAdmin,
-             ContributorsAdmin,
-             AffiliationsAdmin,
-             RawUnpaywallAdmin,
-             SupplementaryDataAdmin,
+    items = (JournalAdmin,
+             ScholarlyArticleAdmin,
+             ContributorAdmin,
+             AffiliationAdmin,
+             RawRecordAdmin,
              ErrorLogAdmin)
 
 
-modeladmin_register(ScholarlyArticlesAdminGroup)
+modeladmin_register(ScholarlyArticleAdminGroup)
