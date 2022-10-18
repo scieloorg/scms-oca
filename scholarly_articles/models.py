b4deed6321761ce1c84cdb1f09a971bfda307c6d
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-10-18 14:57:27 -0300

    Ajustes nos modelos

--- scholarly_articles/models.py
+++ scholarly_articles/models.py
@@ -8,7 +8,7 @@ from . import choices
 from core.models import CommonControlField
 
 
-class ScholarlyArticles(models.Model):
+class ScholarlyArticle(models.Model):
     doi = models.CharField(_("DOI"), max_length=100, null=True, blank=True)
     title = models.CharField(_("Title"), max_length=510, null=True, blank=True)
     volume = models.CharField(_("Volume"), max_length=20, null=True, blank=True)
@@ -19,15 +19,15 @@ class ScholarlyArticles(models.Model):
                                      blank=True)
     use_license = models.CharField(_("Use License"), max_length=50, choices=choices.LICENSE, null=True, blank=True)
     apc = models.CharField(_("Article Processing Charge"), max_length=20, choices=choices.APC, null=True, blank=True)
-    contributors = models.ManyToManyField("Contributors", verbose_name=_("Contributors"), blank=True)
-    journal = models.ForeignKey("Journals", verbose_name=_("Journals"), on_delete=models.SET_NULL, max_length=255, null=True, blank=True)
-    source = models.CharField(_("Record Source"), max_length=50, choices=choices.SOURCE, null=True, blank=True)
+    contributors = models.ManyToManyField("Contributor", verbose_name=_("Contributor"), blank=True)
+    journal = models.ForeignKey("Journal", verbose_name=_("Journals"), on_delete=models.SET_NULL, max_length=255, null=True, blank=True)
+    source = models.CharField(_("Record Source"), max_length=50, null=True, blank=True)
 
     def __unicode__(self):
-        return self.doi if self.doi else ""
+        return self.doi or ""
 
     def __str__(self):
-        return self.doi if self.doi else ""
+        return self.doi or ""
 
     class Meta:
         indexes = [
@@ -49,18 +49,19 @@ class ScholarlyArticles(models.Model):
     ]
 
 
-class Journals(models.Model):
+class Journal(models.Model):
     journal_issn_l = models.CharField(_("ISSN-L"), max_length=50, null=True, blank=True)
     journal_issns = models.CharField(_("ISSN's"), max_length=50, null=True, blank=True)
     journal_name = models.CharField(_("Journal Name"), max_length=255, null=True, blank=True)
     publisher = models.CharField(_("Publisher"), max_length=255, null=True, blank=True)
     journal_is_in_doaj = models.BooleanField(_("DOAJ"), default=False, null=True, blank=True)
+    source = models.CharField(_("Source"), max_length=50, null=True, blank=True)
 
     def __unicode__(self):
-        return self.journal_issn_l if self.journal_issn_l else ""
+        return self.journal_issn_l or ""
 
     def __str__(self):
-        return self.journal_issn_l if self.journal_issn_l else ""
+        return self.journal_issn_l or ""
 
     class Meta:
         indexes = [
@@ -75,28 +76,29 @@ class Journals(models.Model):
         FieldPanel('journal_name'),
         FieldPanel('publisher'),
         FieldPanel('journal_is_in_doaj'),
+        FieldPanel('source'),
     ]
 
 
-class Contributors(models.Model):
+class Contributor(models.Model):
     family = models.CharField(_("Family Name"), max_length=255, null=True, blank=True)
     given = models.CharField(_("Given Name"), max_length=255, null=True, blank=True)
     orcid = models.CharField("ORCID", max_length=50, null=True, blank=True)
     authenticated_orcid = models.BooleanField(_("Authenticated"), default=False, null=True, blank=True)
-    affiliation = models.ForeignKey('Affiliations', on_delete=models.SET_NULL, max_length=510, null=True, blank=True)
+    affiliation = models.ManyToManyField('Affiliation', blank=True)
+    source = models.CharField(_("Source"), max_length=50, null=True, blank=True)
 
     def __unicode__(self):
-        return f"{self.family}, {self.given} ({self.orcid})"
+        return f"{self.family}, {self.given} ({self.orcid})" or ""
 
     def __str__(self):
-        return f"{self.family}, {self.given} ({self.orcid})"
+        return f"{self.family}, {self.given} ({self.orcid})" or ""
 
     class Meta:
         indexes = [
             models.Index(fields=['family', ]),
             models.Index(fields=['given', ]),
             models.Index(fields=['orcid', ]),
-            models.Index(fields=['affiliation', ]),
         ]
 
     panels = [
@@ -108,16 +110,17 @@ class Contributors(models.Model):
     ]
 
 
-class Affiliations(models.Model):
+class Affiliation(models.Model):
     name = models.CharField(_("Affiliation Name"), max_length=510, null=True, blank=True)
+    source = models.CharField(_("Source"), max_length=50, null=True, blank=True)
     official = models.ForeignKey(Institution, verbose_name=_("Official Affiliation Name"), on_delete=models.SET_NULL,
                                  max_length=1020, null=True, blank=True)
 
     def __unicode__(self):
-        return self.name if self.name else ""
+        return self.name or ""
 
     def __str__(self):
-        return self.name if self.name else ""
+        return self.name or ""
 
     class Meta:
         indexes = [
@@ -130,22 +133,20 @@ class Affiliations(models.Model):
     ]
 
 
-class RawUnpaywall(models.Model):
+class RawRecord(models.Model):
     doi = models.CharField(_("DOI"), max_length=100, null=False, blank=False)
     harvesting_creation = models.CharField(_("Harvesting date"), max_length=20, null=False, blank=False)
-    is_paratext = models.BooleanField(_("Paratext"), default=False, null=True, blank=True)
     year = models.CharField(_("Year"), max_length=10, null=True, blank=True)
-    # unpaywall genre
     resource_type = models.CharField(_("Resource Type"), max_length=50, choices=choices.TYPE_OF_RESOURCE, null=False,
                                      blank=True)
-    update = models.CharField(_("Update"), max_length=20, null=True, blank=True)
+    source = models.CharField(_("Source"), max_length=100, null=False, blank=False)
     json = models.JSONField(_("JSON File"), null=True, blank=True)
 
     def __unicode__(self):
-        return self.doi
+        return f"{self.doi} ({self.source})" or ""
 
     def __str__(self):
-        return self.doi
+        return f"{self.doi} ({self.source})" or ""
 
     class Meta:
         indexes = [
@@ -154,39 +155,15 @@ class RawUnpaywall(models.Model):
             models.Index(fields=['resource_type', ]),
         ]
 
-    panels = [
-        FieldPanel('doi'),
-        FieldPanel('harvesting_creation'),
-        FieldPanel('is_paratext'),
-        FieldPanel('year'),
-        FieldPanel('resource_type'),
-        FieldPanel('update'),
-        FieldPanel('json'),
-    ]
-
-
-class SupplementaryData(models.Model):
-    doi = models.CharField(_("DOI"), max_length=100, null=False, blank=False)
-    year = models.CharField(_("Year"), max_length=10, null=True, blank=True)
-    json = models.JSONField(_("JSON File"), null=True, blank=True)
-
-    def __unicode__(self):
-        return self.doi
-
-    def __str__(self):
-        return self.doi
-
-    class Meta:
-        indexes = [
-            models.Index(fields=['doi', ]),
-            models.Index(fields=['year', ]),
-        ]
-
-    panels = [
-        FieldPanel('doi'),
-        FieldPanel('year'),
-        FieldPanel('json'),
-    ]
+    # panels = [
+    #     FieldPanel('doi'),
+    #     FieldPanel('harvesting_creation'),
+    #     FieldPanel('is_paratext'),
+    #     FieldPanel('year'),
+    #     FieldPanel('resource_type'),
+    #     FieldPanel('update'),
+    #     FieldPanel('json'),
+    # ]
 
 
 class ErrorLog(CommonControlField):
