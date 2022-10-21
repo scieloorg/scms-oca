035fad8845dafc6358d32b9be5af09eb7d4bffa0
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-10-21 13:06:37 -0300

    Adiciona 'source' na interface

--- scholarly_articles/wagtail_hooks.py
+++ scholarly_articles/wagtail_hooks.py
@@ -105,6 +105,7 @@ class AffiliationsAdmin(ModelAdmin):
     list_display = (
         'name',
         'official',
+        'source',
     )
     #list_filter = ('name',)
     search_fields = ('name', 'official',)
