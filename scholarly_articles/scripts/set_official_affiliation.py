e2443bcc2683080e6f259fd1b86f68ce6baf1497
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-10-21 15:52:23 -0300

    Corrige 'official_affiliation.py'

--- scholarly_articles/scripts/set_official_affiliation.py
+++ scholarly_articles/scripts/set_official_affiliation.py
@@ -4,6 +4,6 @@ from institution import models as institution_models
 
 def run():
     for official in institution_models.Institution.objects.filter(source='MEC').iterator():
-        for aff in article_models.Affiliations.objects.filter(name__upper__contains=official.upper()).iterator():
+        for aff in article_models.Affiliations.objects.filter(name__icontains=official.name).iterator():
              aff.official = official
              aff.save()
