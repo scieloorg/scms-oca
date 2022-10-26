abacd109b0e8acdd879f68cfa2a859ee91e43c1a
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-10-26 11:47:40 -0300

    Adiciona 'bulk_countries.py'

---
+++ usefulmodels/scripts/bulk_countries.py
@@ -0,0 +1,29 @@
+import os
+from usefulmodels import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# This script add bulk of countries
+# This presuppose a fixtures/countries.csv file exists.
+# Consider that existe a user with id=1
+
+SEPARATOR = ';'
+
+def run(*args):
+    user_id = 1
+
+    # Delete all cities
+    models.Country.objects.all().delete()
+
+    with open(os.path.dirname(os.path.realpath(__file__)) + "/../fixtures/countries.csv", 'r') as fp:
+        for line in fp.readlines():
+            name_pt, name_en, capital, acron3, acron2 = line.strip().split(SEPARATOR)
+
+            # User
+            if args:
+                user_id = args[0]
+
+            creator = User.objects.get(id=user_id)
+
+            models.Country(name_pt=name_pt, name_en=name_en, acronym=acron3, creator=creator).save()
