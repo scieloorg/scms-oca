1830498601935951ae7d307d7b100e801aee4253
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-10-26 11:44:35 -0300

    Refatora 'set_country.py'

--- scholarly_articles/scripts/set_country.py
+++ scholarly_articles/scripts/set_country.py
@@ -2,12 +2,15 @@ import os
 from scholarly_articles import models
 from django.contrib.auth import get_user_model
 
+from usefulmodels.models import Country
+
+
 User = get_user_model()
 
 SEPARATOR = ';'
 
 def get_country(country):
-    return models.Affiliations.objects.filter(name__icontains=country)
+    return models.Affiliations.objects.filter(name__icontains=country, country__isnull=True)
 
 
 def run(*args):
@@ -23,9 +26,11 @@ def run(*args):
 
             creator = User.objects.get(id=user_id)
 
-            for country in [name_pt, name_en]:
-                affiliations = get_country(country)
+            search_for = [name_pt, name_en]
+            for pos in range(len(search_for)):
+                affiliations = get_country(search_for[pos])
                 for affiliation in affiliations.iterator():
-                    if not affiliation.country:
-                        affiliation.country = name_pt
-                        affiliation.save()
+                    affiliation.country = Country.get_or_create(user=creator, name_pt=search_for[pos]) \
+                        if pos == 0 \
+                        else Country.get_or_create(user=creator, name_en=search_for[pos])
+                    affiliation.save()
