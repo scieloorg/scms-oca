9f4ca29665dece203bd8effc26f2dd3e13b374c9
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-11-02 12:23:46 -0300

    Insere arquivo removido erroneamente

---
+++ infrastructure_directory/models.py
@@ -0,0 +1,125 @@
+import os
+
+from django.db import models
+from django.utils.translation import gettext as _
+from taggit.managers import TaggableManager
+from wagtail.admin.edit_handlers import FieldPanel, HelpPanel
+from wagtail.documents.edit_handlers import DocumentChooserPanel
+from wagtailautocomplete.edit_handlers import AutocompletePanel
+
+from core.models import CommonControlField
+from institution.models import Institution
+from usefulmodels.models import ThematicArea, Practice, Action
+
+from . import choices
+from .forms import InfrastructureDirectoryFileForm, InfrastructureDirectoryForm
+
+
+class InfrastructureDirectory(CommonControlField):
+    class Meta:
+        verbose_name_plural = _('Infraestructure Directory')
+
+    title = models.CharField(_("Title"), max_length=255, null=False, blank=False)
+    link = models.URLField(_("Link"), null=False, blank=False)
+    description = models.TextField(_("Description"), max_length=1000,
+                                   null=True, blank=True)
+
+    institutions = models.ManyToManyField(Institution, verbose_name=_("Institution"), blank=True)
+    thematic_areas = models.ManyToManyField(ThematicArea, verbose_name=_("Thematic Area"), blank=True)
+
+    practice = models.ForeignKey(Practice, verbose_name=_("Practice"),
+                                 null=True, blank=True, on_delete=models.SET_NULL)
+    action = models.ForeignKey(Action, verbose_name=_("Action"), null=True, blank=True, on_delete=models.SET_NULL)
+
+
+    classification = models.CharField(_("Classification"), choices=choices.classification,
+                                      max_length=255, null=True, blank=True)
+
+    keywords = TaggableManager(_("Keywords"), blank=True)
+
+    record_status = models.CharField(_("Record status"), choices=choices.status,
+                                     max_length=255, null=True, blank=True)
+
+    source = models.CharField(_("Source"), max_length=255, null=True, blank=True)
+
+    panels = [
+        HelpPanel('Portais, plataformas, servidores, repositórios e serviços brasileiros que operam em acesso aberto objetos de comunicação de comunicação de pesquisas, recursos de apoio e resultantes de pesquisas e em acesso aberto.'),
+        FieldPanel('title'),
+        FieldPanel('link'),
+        FieldPanel('source'),
+
+        FieldPanel('description'),
+        AutocompletePanel('institutions'),
+
+        AutocompletePanel('thematic_areas'),
+        FieldPanel('keywords'),
+        FieldPanel('classification'),
+        FieldPanel('practice'),
+
+        FieldPanel('source'),
+        FieldPanel('record_status'),
+    ]
+
+    def __unicode__(self):
+        return u'%s' % self.title
+
+    def __str__(self):
+        return u'%s' % self.title
+
+    @property
+    def data(self):
+        d = {
+            "infrastructure__title": self.title,
+            "infrastructure__link": self.link,
+            "infrastructure__description": self.description,
+            "infrastructure__classification": self.classification,
+            "infrastructure__keywords": [keyword for keyword in self.keywords.iterator()],
+            "infrastructure__record_status": self.record_status,
+            "infrastructure__source": self.source
+        }
+
+        if self.institutions:
+            inst = []
+            for institution in self.institutions.iterator():
+                inst.append(institution.data)
+            d.update({"infrastructure__institutions": inst})
+
+        if self.thematic_areas:
+            area = []
+            for thematic_area in self.thematic_areas.iterator():
+                area.append(thematic_area.data)
+            d.update({"infrastructure__thematic_areas": area})
+
+        if self.practice:
+            d.update(self.practice.data)
+
+        if self.action:
+            d.update(self.action.data)
+
+        return d
+
+    base_form_class = InfrastructureDirectoryForm
+
+class InfrastructureDirectoryFile(CommonControlField):
+    class Meta:
+        verbose_name_plural = _('Infraestructure Directory Upload')
+
+    attachment = models.ForeignKey(
+        'wagtaildocs.Document',
+        verbose_name=_("Attachement"),
+        null=True, blank=False,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    is_valid = models.BooleanField(_("Is valid?"), default=False, blank=True,
+                                   null=True)
+    line_count = models.IntegerField(_("Number of lines"), default=0,
+                                     blank=True, null=True)
+
+    def filename(self):
+        return os.path.basename(self.attachment.name)
+
+    panels = [
+        DocumentChooserPanel('attachment')
+    ]
+    base_form_class = InfrastructureDirectoryFileForm
