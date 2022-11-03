5200f09e66ccb79bdc432656bd7e00ac4f31b14f
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-11-03 07:07:00 -0300

    Adiciona segundo ciclo de iterações
    Nesse ciclo é utilizado o acrônimo do país com três caracteres

--- scholarly_articles/scripts/set_country.py
+++ scholarly_articles/scripts/set_country.py
@@ -5,9 +5,19 @@ from usefulmodels.models import Country
 
 
 def run():
+    #first iteration to identify country by name
     for country in Country.objects.all():
         for aff in models.Affiliations.objects.filter(
                 Q(name__icontains=country.name_en) | Q(name__icontains=country.name_pt),
                   country__isnull=True, official__isnull=True).iterator():
+            aff.country = country
+            aff.save()
+
+    #second iteration to identify country by acronym with 3 char
+    for country in Country.objects.all():
+        for aff in models.Affiliations.objects.filter(
+                name__icontains=country.acron3,
+                country__isnull=True,
+                official__isnull=True).iterator():
             aff.country = country
             aff.save()
