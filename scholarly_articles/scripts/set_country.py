4d710135b0f9e102301fb9b326c490aa91b56a92
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-11-08 11:02:57 -0300

    Adiciona a verificação dos registros 'ROR'

--- scholarly_articles/scripts/set_country.py
+++ scholarly_articles/scripts/set_country.py
@@ -2,10 +2,17 @@ from django.db.models import Q
 
 from scholarly_articles import models
 from usefulmodels.models import Country
+from institution.models import Institution
 
 
 def run():
-    #first iteration to identify country by name
+    #first iteration to identify country by institution ROR
+    for institution_ror in Institution.objects.filter(source="ROR").iterator():
+        for aff in models.Affiliations.objects.filter(name__icontains=institution_ror.name,
+                                                      country__isnull=True).iterator():
+            aff.country = institution_ror.location.country
+
+    #second iteration to identify country by declared name
     for country in Country.objects.all():
         for aff in models.Affiliations.objects.filter(
                 Q(name__icontains=country.name_en) | Q(name__icontains=country.name_pt),
@@ -13,9 +20,9 @@ def run():
             aff.country = country
             aff.save()
 
-        #second iteration to identify country by acronym with 3 char
+        #second iteration to identify country by declared acronym with 3 char
         for aff in models.Affiliations.objects.filter(
-                name__icontains=country.acron3,
+                Q(name__contains=" "+country.acron3+" ") | Q(name__contains=" "+country.acron3),
                 country__isnull=True,
                 official__isnull=True).iterator():
             aff.country = country
