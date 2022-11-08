1cef4a001115d7fe77fcd5c1a17d3c2cc060b252
Roberta Takenaka <roberta.takenaka@scielo.org>
2022-11-08 18:29:53 -0300

    Cria rotas para os detalhes dos indicadores e para descarregar o dataset completo ou computado

--- search/urls.py
+++ search/urls.py
@@ -6,4 +6,7 @@ app_name = "search"
 urlpatterns = [
     # path /
     path("", views.search, name="search"),
+    path("indicator/<int:indicator_id>/detail/", views.indicator_detail, name='indicator_detail'),
+    path("indicator/<int:indicator_id>/summarized/", views.indicator_summarized, name='indicator_summarized'),
+    path("indicator/<int:indicator_id>/raw_data/", views.indicator_raw_data, name='indicator_raw_data'),
 ]
