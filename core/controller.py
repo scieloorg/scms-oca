0f14293e79c7be052a790e3d587c7fec1225cedb
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2022-12-10 15:30:42 -0300

    Corrige 'controller.py'

--- core/controller.py
+++ core/controller.py
@@ -12,30 +12,27 @@ import os
 def check_values():
     models = (EducationDirectory, EventDirectory, InfrastructureDirectory, PolicyDirectory)
     fields = ('locations', 'institutions', 'attendance', 'description')
-    d = []
-    for model in models:
-        for field in fields:
-            try:
-                if field == 'locations':
-                    records = model.objects.filter(locations=None)
-                elif field == 'institutions':
-                    records = model.objects.filter(institutions=None)
-                elif field == 'attendance':
-                    records = model.objects.filter(attendance=None)
-                else:
-                    records = model.objects.filter(description=None)
-                for item in records.iterator():
-                    d.append(
-                        {'model': model.__name__,
-                         'field_missing': field,
-                         'register_id': item.id,
-                         'register_text': item.__str__()
-                         }
-                    )
-            except FieldError:
-                pass
 
-    with open(os.path.dirname(os.path.realpath(__file__)) + "/./fixtures/missing_values.csv", 'w') as csvfile:
+    with open(os.path.dirname(os.path.realpath(__file__)) + "/./media/missing_values.csv", 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=['model', 'field_missing', 'register_id', 'register_text'])
-        writer.writeheader()
-        writer.writerows(d)
+
+        for model in models:
+            for field in fields:
+                try:
+                    if field == 'locations':
+                        records = model.objects.filter(locations=None)
+                    elif field == 'institutions':
+                        records = model.objects.filter(institutions=None)
+                    elif field == 'attendance':
+                        records = model.objects.filter(attendance=None)
+                    else:
+                        records = model.objects.filter(description=None)
+                    for item in records.iterator():
+                        writer.writerow({
+                            'model': model.__name__,
+                            'field_missing': field,
+                            'register_id': item.id,
+                            'register_text': str(item)
+                        })
+                except FieldError:
+                    pass
