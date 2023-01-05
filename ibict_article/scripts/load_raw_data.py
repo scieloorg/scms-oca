2251f5022f96bf0a00917a0cbede706801aaf8c0
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-01-05 18:39:03 -0300

    Adiciona 'scripts/'

---
+++ ibict_article/scripts/load_raw_data.py
@@ -0,0 +1,9 @@
+from ibict_article.tasks import load_raw_data
+
+
+def run(*args):
+
+    if args:
+        load_raw_data.apply_async(kwargs={"file_path": args[0]})
+    else:
+        print("The path to unpaywall file is required.")
