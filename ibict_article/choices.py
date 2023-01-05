c6a79895c494be7bbc1e98674aa7611e4cb4f975
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-01-05 18:38:12 -0300

    Adiciona 'choices.py'

---
+++ ibict_article/choices.py
@@ -0,0 +1,9 @@
+from django.utils.translation import gettext as _
+
+TYPES = [
+    ('', ''),
+    ('conference proceedings', _('Conference Proceedings')),
+    ('journal article', _('Journal Article')),
+    ('master thesis', _('Master Thesis')),
+    ('doctoral thesis', _('Doctoral Thesis')),
+]
