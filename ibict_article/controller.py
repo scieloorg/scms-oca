f452bcba5d2ea6a1ce21e7634b39d4444595f17c
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-01-05 18:42:55 -0300

    Adiciona procedimento para popular 'RawArticle'

---
+++ ibict_article/controller.py
@@ -0,0 +1,24 @@
+import orjson
+
+from .models import RawArticle, JournalArticle, Thesis, ConferenceProceedings
+from .core import Authorship, GenericArticle
+
+
+def load_raw_ibict(row, user):
+    row = orjson.loads(row)
+    document_type = row.get('fields').get('type')[0]
+    document_id = row.get('entity_id')
+
+    try:
+        rawrecord = RawArticle.objects.filter(entity_id=document_id, document_type=document_type)
+        if rawrecord:
+            pass
+        else:
+            rawrecord = RawArticle()
+            rawrecord.document_type = document_type
+            rawrecord.entity_id = document_id
+            rawrecord.json = row
+            rawrecord.creator = user
+            rawrecord.save()
+    except Exception as e:
+        print(str(e))
