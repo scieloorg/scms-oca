f2ef64948cc1e4ca53dcee38327b2771fe2965aa
Luciano Rossi <luciano.rossi.lucross@gmail.com>
2023-01-05 18:44:48 -0300

    Adiciona função que retorna valores dos atributos

--- ibict_article/controller.py
+++ ibict_article/controller.py
@@ -31,3 +31,43 @@ def get_value_in_a_list(list_of_values):
         return
 
 
+def get_generic_article_values(user, json):
+    entity_id = json.get('entity_id')
+    keyword = json.get('fields').get('keyword')  # it's a list
+    document_title = json.get('fields').get('title')  # it's a list
+    authors = []
+    for author in json.get('relations').get('Authorship') or []:
+        orcid = get_value_in_a_list(author.get('fields').get('identifier.orcid'))
+        id_lattes = get_value_in_a_list(author.get('fields').get('identifier.lattes'))
+        names = author.get('fields').get('name')  # it's a list
+        citation_names = author.get('fields').get('citationName')  # it's a list
+        research_areas = author.get('fields').get('researchArea')  # it's a list
+        birth_city = get_value_in_a_list(author.get('fields').get('birthCity'))
+        birth_state = get_value_in_a_list(author.get('fields').get('birthState'))
+        birth_country = get_value_in_a_list(author.get('fields').get('birthCountry'))
+        try:
+            authors.append(
+                Authorship.authorship_get_or_create(
+                    user=user,
+                    orcid=orcid,
+                    id_lattes=id_lattes,
+                    names=names,
+                    citation_names=citation_names,
+                    research_areas=research_areas,
+                    birth_city=birth_city,
+                    birth_state=birth_state,
+                    birth_country=birth_country
+                )
+            )
+        except:
+            pass
+    publication_date = get_value_in_a_list(json.get('fields').get('publicationDate'))
+    document_type = get_value_in_a_list(json.get('fields').get('type'))
+    language = get_value_in_a_list(json.get('fields').get('language'))
+    research_area = json.get('fields').get('researchArea.cnpq')  # it's a list
+    start_page = get_value_in_a_list(json.get('fields').get('starPage'))
+    end_page = get_value_in_a_list(json.get('fields').get('endPage'))
+    volume = get_value_in_a_list(json.get('fields').get('volume'))
+
+    return entity_id, keyword, document_title, authors, publication_date, document_type, language, research_area, \
+        start_page, end_page, volume
