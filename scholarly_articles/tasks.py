5a8b398e008fde2964918b492316fea20b5a87da
Samuel Veiga <samuelveiga@pop-os.localdomain>
2023-03-28 11:13:31 -0300

    Inserido API Crossref como var de ambiente e inserido comentarios na funcao load_crossref

--- scholarly_articles/tasks.py
+++ scholarly_articles/tasks.py
@@ -13,8 +13,11 @@ from scholarly_articles.unpaywall import (
 from scholarly_articles.crossref import (
     crossref,
     fetch_data_retry,
-    
 )
+from config.settings.base import (
+    URL_API_CROSSREF,
+)
+
 
 import logging
 
@@ -102,24 +105,96 @@ def load_crossref(from_update_date, until_update_date):
 
     Sync or Async
     Param: from_update_date and until_update_date are strings representing the date range in the format 'YYYY'.
-    """
-    url = f'https://api.crossref.org/works?query.affiliation=Brazil&filter=type:journal-article,from-update-date:{from_update_date},until-update-date:{until_update_date}&cursor=*'
     
-    dados = ['DOI', 'title', 'volume', 'publisher', 'source', 'type',]
+    The crossref API format is something like the format below:
+
+    "message": {
+        "facets": {
+            
+        },
+        "next-cursor": "DnF1ZXJ5VGhlbkZldGNoBgAAAAAKerflFjFGV1lXWE94VGU2bUMzaVRjNzc2aFEAAAAAIGt0aRZHblpDbkEzT1FvRy0tOGFOZ05EOHVnAAAAACBrdGgWR25aQ25
+            BM09Rb0ctLThhTmdORDh1ZwAAAAAZqMEuFk5qWk1fWm1iUVV1V3Nwd3MxN3FqQ3cAAAAAFV5fOBZfNEZlTDBRZ1FuLWZ4NVhlZU9MYzN3AAAAABWPAdcWaDRJMjhrcXVUOGE3dDI1VDhnWHMyZw==",
+        "total-results": 37342,
+        "items": [
+            {
+            "indexed": {
+                "date-parts": [[2022,4,5]],
+                "date-time": "2022-04-05T02:58:23Z",
+                "timestamp": 1649127503838
+            },
+            "reference-count": 0,
+            "publisher": "FapUNIFESP (SciELO)",
+            "issue": "4",
+            "content-domain": {
+                "domain": [],
+                "crossmark-restriction": false
+            },
+            "short-container-title": [
+                "Rev. IBRACON Estrut. Mater."
+            ],
+            "published-print": {
+                "date-parts": [[2017,8]]
+            },
+            "DOI": "10.1590/s1983-41952017000400001",
+            "type": "journal-article",
+            "created": {
+                "date-parts": [[2017,8,31]],
+                "date-time": "2017-08-31T17:37:46Z",
+                "timestamp": 1504201066000
+            },
+            "page": "787-787",
+            "source": "Crossref",
+            "is-referenced-by-count": 0,
+            "title": [
+                "Editorial"
+            ],
+            "prefix": "10.1590",
+            "volume": "10",
+            "author": [
+                {
+                "given": "Américo",
+                "family": "Campos Filho",
+                "sequence": "first",
+                "affiliation": [
+                    {
+                    "name": ",  Brazil"
+                    }
+                ]
+                },
+            ],
+            "member": "530",
+            "container-title": [
+                "Revista IBRACON de Estruturas e Materiais"
+            ],
+        ]
+    }
+    """
+    url = URL_API_CROSSREF + f'?query.affiliation=Brazil&filter=type:journal-article,from-update-date:{from_update_date},until-update-date:{until_update_date}&cursor=*'
+    
+    dados = [
+        'publisher', 
+        'DOI', 
+        'type', 
+        'source', 
+        'title', 
+        'volume', 
+        'author', 
+        'container-title'
+    ]
 
     try:
         while True:
 
-            data = fetch_data_retry.request_retry(url)
+            data = fetch_data_retry.request_retry(url, total=10, backoff_factor=1)
             articles = []
-
             if data['status'] == 'ok':
                 for item in data['message']['items']:
                     article_dict = {field: item.get(field, '') for field in dados}
                     articles.append(article_dict)
 
+                # If there were no more items, the interaction stops
                 if not data['message']['items']:
-                    return "No articles found."
+                    return "No more articles found."
 
                 # Send articles to a function that will save in the database
                 crossref.load(articles)
