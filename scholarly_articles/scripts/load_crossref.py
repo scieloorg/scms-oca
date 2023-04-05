4f74e059db5449da6fc8393ba9403fe986000422
GitInno <86991526+gitnnolabs@users.noreply.github.com>
2023-04-05 09:13:10 -0300

    Adiciona uma docstring.

--- scholarly_articles/scripts/load_crossref.py
+++ scholarly_articles/scripts/load_crossref.py
@@ -1,8 +1,11 @@
 from scholarly_articles.tasks import load_crossref
 
+
 def run(from_update_date=2012, until_update_date=2012):
+    """
+    Load the article from crossref API to ScholarlyArticles.
+    """
     if from_update_date and until_update_date:
         load_crossref.apply_async(args=(from_update_date, until_update_date))
     else:
-        print('from_update_date and until_update_date is required.')
-    
+        print("from_update_date and until_update_date is required.")
