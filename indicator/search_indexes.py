826bed989fee25539f2c96f36fb2967912dacad7
GitInno <86991526+gitnnolabs@users.noreply.github.com>
2023-05-04 09:22:07 -0300

    Remove comentário do atributo priority.

--- indicator/search_indexes.py
+++ indicator/search_indexes.py
@@ -55,7 +55,7 @@ class IndicatorIndex(indexes.SearchIndex, indexes.Indexable):
     # nacional -> municipal
     geo_priority = indexes.IntegerField(null=True)
     thematic_priority = indexes.IntegerField(null=True)
-    # priority = indexes.FloatField(null=True)
+    priority = indexes.FloatField(null=True)
 
     # scopes
     # por enquanto deixar apenas geo_scope, traduzir apenas como "âmbito"
@@ -160,8 +160,8 @@ class IndicatorIndex(indexes.SearchIndex, indexes.Indexable):
             if thematic_area.level0:
                 return "level0"
 
-    # def prepare_priority(self, obj):
-    #     return obj.raw_data and obj.raw_data.size
+    def prepare_priority(self, obj):
+        return obj.raw_data and obj.raw_data.size
 
     def prepare_action(self, obj):
         return obj.action_and_practice and obj.action_and_practice.action.name
@@ -175,9 +175,6 @@ class IndicatorIndex(indexes.SearchIndex, indexes.Indexable):
 
     def prepare_practice(self, obj):
         return obj.action_and_practice and obj.action_and_practice.practice.name
-
-    # def prepare_file_csv(self, obj):
-    #     return obj.raw_data and obj.raw_data.url
 
     def prepare_record_type(self, obj):
         return "indicator"
