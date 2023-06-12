367e8e48e715c0c823c3ffdcd467b19337aba7fd
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-06-12 13:25:10 -0300

    Adiciona métodos em Indicator

--- indicator/models.py
+++ indicator/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 import logging
 import os
@@ -22,6 +23,14 @@ from usefulmodels.models import ActionAndPractice, ThematicArea
 from . import choices
 from .forms import IndicatorDirectoryForm
 from .permission_helper import MUST_BE_MODERATE
+
+
+class GetOrCreateCrontabScheduleError(Exception):
+    ...
+
+
+class CreateIndicatorRecordError(Exception):
+    ...
 
 
 class Indicator(CommonControlField):
@@ -125,10 +134,14 @@ class Indicator(CommonControlField):
         return f"/indicator/indicator/edit/{self.id}/"
 
     @property
-    def header(self):
-        link = "https://ocabr.org/search/indicator/{}/detail/".format(
-            self.id,
+    def permanent_link(self):
+        return "https://ocabr.org/search/indicator/{}/detail/".format(
+            self.slug
         )
+
+    @property
+    def header(self):
+        link = self.permanent_link
         d = dict(
             title=self.title,
             description=self.description,
@@ -272,14 +285,6 @@ class Indicator(CommonControlField):
         FieldPanel("notes", permission="indicator.can_edit_notes"),
     ]
 
-    # https://drive.google.com/drive/folders/1_J8iKhr_gayuBqtvnSWreC-eBnxzY9rh
-    # IDENTIDADE sugerido:
-    #      (seq + action + classification) +
-    #      (created + creator_id) +
-    #      (validity + previous + posterior) +
-    #      (title)
-    # ID melhorado:
-    #    action + classification + practice + scope + seq
     def __unicode__(self):
         return f"{self.title} {self.seq} {self.validity} {self.updated}"
 
@@ -296,3 +301,373 @@ class Indicator(CommonControlField):
         return slugify("_".join(items).lower())
 
     base_form_class = IndicatorDirectoryForm
+
+    @classmethod
+    def get(cls, key, **kwargs):
+        """
+        Retorna Indicador com qualquer chave.
+        Sugiro testar indicator.id == key e fazer redirect.
+
+        Raises
+        ------
+        cls.DoesNotExist
+        """
+        return cls.objects.get(Q(slug=key) | Q(code=key) | Q(id=key), **kwargs)
+
+    @property
+    def name(self):
+        """
+        Gera parte do título: "Número de XXXx"
+        """
+        # Número de | Evolução do número de | Porcentagem de
+        d = dict(choices.MEASUREMENT_TYPE)
+        d["FREQUENCY"] = _("Número")
+        d["EVOLUTION"] = _("Evolução do número")
+
+        measurement_title = d.get(self.measurement) + " " + _("de")
+
+        # políticas | ações | journal-article
+        object_name = _(
+            self.action_and_practice
+            and self.action_and_practice.action
+            and self.action_and_practice.action.name
+            or self.object_name
+        )
+        return f"{measurement_title} {object_name}"
+
+    @classmethod
+    def build_old_code(
+        cls,
+        action,
+        classification,
+        practice,
+        measurement,
+        object_name,
+        start_date_year,
+        end_date_year,
+        category1_id,
+        category2_id,
+        context,
+    ):
+        items = [
+            action and action.code or "",
+            slugify(classification) or "",
+            practice and practice.code or "",
+            measurement,
+            object_name,
+            category2_id or category1_id or "",
+            str(start_date_year or ""),
+            str(end_date_year or ""),
+        ] + (context or [])
+        return _str_with_64_char(slugify("_".join(items)).upper())
+
+    @classmethod
+    def get_latest_version_by_code(cls, code):
+        """
+        Obtém a versão mais recente de uma instância de Indicator,
+
+        Parameters
+        ----------
+        code : str
+        """
+        try:
+            return cls.objects.filter(code=code).latest("created")
+        except cls.DoesNotExist as e:
+            return None
+
+    @classmethod
+    def get_latest_version(
+        cls,
+        source,
+        object_name,
+        measurement,
+        category=None,
+        context=None,
+        start_date_year=None,
+        end_date_year=None,
+        action=None,
+        classification=None,
+        practice=None,
+    ):
+        action_and_practice = None
+        if action or classification or practice:
+            action_and_practice = ActionAndPractice.get_or_create(
+                action, classification, practice
+            )
+        try:
+            return cls.objects.filter(
+                source=source,
+                object_name=object_name,
+                measurement=measurement,
+                category=category,
+                context=context,
+                start_date_year=start_date_year,
+                end_date_year=end_date_year,
+                action_and_practice=action_and_practice,
+                posterior_record__isnull=True,
+            ).latest("created")
+        except cls.DoesNotExist as e:
+            return None
+
+    @classmethod
+    def get_latest_version_by_slug(cls, slug):
+        """
+        Obtém a versão mais recente de uma instância de Indicator,
+
+        Parameters
+        ----------
+        slug : str
+        """
+        try:
+            return cls.objects.get(slug=slug, posterior_record__isnull=True)
+        except cls.DoesNotExist as exc:
+            try:
+                curr = cls.objects.get(slug=slug)
+            except cls.DoesNotExist as exc:
+                return None
+            else:
+                while True:
+                    next_ = curr.posterior_record
+                    if not next_:
+                        return curr
+                    curr = next_
+
+    def generate_title(self):
+        name = self.name
+
+        # por área temática | por instit | por UF
+        by_category = ""
+        if self.category:
+            by_category = " " + _("por") + " " + _(self.category)
+
+        # ano ou intervalo de anos
+        years = ""
+        if self.start_date_year:
+            years = f" - {self.start_date_year.isoformat()[:10]}"
+            if self.end_date_year and self.start_date_year != self.end_date_year:
+                years += f"-{self.start_date_year.isoformat()[:10]}"
+
+        # contexto institucional, geográfico, temático
+        # Universidade X | SP | Ciências Biológicas
+        context = self.context and f" - {self.context}" or ""
+
+        return f"{name}{by_category}{years}{context}"
+
+    def set_code(self):
+        """
+        Sugestão
+        https://docs.google.com/document/d/1nOvMLwsePKA5BHOYQiwGOrQDskFsxM4M/edit
+        ----------------------------------------------
+        Identidade = {(ação, versão, postagem, nome)}
+        versão = id, status
+        status = ativo | inativo
+        inativo = anterior, posterior
+        postagem = {(data, autoria)}
+        nome = Número de documentos ...
+        ----------------------------------------------
+        Mas:
+
+        object_name - ex.: políticas | journal-article | ações | ...
+        tipo de indicador - ex.: freq | evol | perc | ...
+        categoria - ex.: por área temática, por instituição, por UF,
+        data início, data fim - data dos dados
+        contexto - UF | Instituição | Área temática
+        fonte - criador do indicador
+        data de criação do indicador
+        """
+        # ação ou objeto (ex.: políticas, journal-article, ações, ...)
+        object_name = (
+            self.action_and_practice
+            and self.action_and_practice.action
+            and self.action_and_practice.action.code
+            or self.object_name
+        )
+
+        items = (
+            object_name,
+            self.measurement[:4],
+            self.category or "",
+            self.context or "",
+            str(self.start_date_year and self.start_date_year.isoformat()[:10] or ""),
+            str(self.end_date_year and self.end_date_year.isoformat()[:10] or ""),
+            self.source or "",
+            self.created.isoformat()[:10],
+        )
+        self.code = slugify("-".join(items)).lower()[:500]
+
+    @classmethod
+    def create(
+        cls,
+        creator,
+        source,
+        object_name,
+        measurement,
+        category=None,
+        context=None,
+        start_date_year=None,
+        end_date_year=None,
+        title=None,
+        action=None,
+        classification=None,
+        practice=None,
+        keywords=None,
+        institutions=None,
+        locations=None,
+        thematic_areas=None,
+        # institutional_contribution=None,
+    ):
+        latest = cls.get_latest_version(
+            source,
+            object_name,
+            measurement,
+            category=category,
+            context=context,
+            start_date_year=start_date_year,
+            end_date_year=end_date_year,
+            action=action,
+            classification=classification,
+            practice=practice,
+        )
+        if latest and latest.record_status == choices.WIP:
+            raise CreateIndicatorRecordError(
+                "Indicator is being generated {} {} {}".format(
+                    latest.code, latest.seq, latest.created
+                )
+            )
+        new = latest and latest.create_new_from_latest(creator)
+        if not new:
+            new = cls()
+            if action or classification or practice:
+                new.action_and_practice = ActionAndPractice.get_or_create(
+                    action, classification, practice
+                )
+            new.measurement = measurement
+            new.object_name = object_name
+            new.source = source
+            new.category = category
+            new.context = context
+            new.start_date_year = start_date_year
+            new.end_date_year = end_date_year
+
+            # new.institutional_contribution = institutional_contribution
+            new.title = title or new.generate_title()
+
+            new.seq = 1
+
+            new.creator = creator
+            new.created = datetime.utcnow()
+            new.save()
+            new.set_code()
+
+            logging.info(type((thematic_areas)))
+            if thematic_areas is not None:
+                new.thematic_areas.set(thematic_areas)
+            if institutions is not None:
+                new.institutions.set(institutions)
+            if locations is not None:
+                new.locations.set(locations)
+            if keywords:
+                new.keywords.add(*keywords)
+
+            if institutions:
+                scope = choices.INSTITUTIONAL
+            elif thematic_areas:
+                scope = choices.THEMATIC
+            elif locations:
+                scope = choices.GEOGRAPHIC
+            else:
+                scope = choices.GENERAL
+            new.scope = scope
+
+        new.record_status = choices.WIP
+        new.previous_record = latest
+        new.save()
+
+        return new
+
+    def create_new_from_latest(self, creator):
+        if not self.posterior_record:
+            return
+
+        new = Indicator()
+        new.measurement = self.measurement
+        new.object_name = self.object_name
+        new.source = self.source
+        new.category = self.category
+        new.context = self.context
+        new.start_date_year = self.start_date_year
+        new.end_date_year = self.end_date_year
+
+        new.description = self.description
+
+        new.scope = self.scope
+        new.action_and_practice = self.action_and_practice
+
+        new.previous_record = self
+
+        new.title = self.title
+        new.seq = self.seq + 1
+
+        new.raw_data = None
+        new.summarized = None
+
+        new.institutional_contribution = None
+        new.link_to_graphic = None
+        new.link_to_data = None
+        new.link = None
+
+        new.slug = None
+        new.record_status = None
+        new.validity = None
+        new.posterior_record = None
+
+        new.creator = creator
+        new.created = datetime.utcnow()
+
+        new.save()
+
+        new.set_code()
+        new.thematic_areas.set(self.thematic_areas)
+        new.institutions.set(self.institutions)
+        new.locations.set(self.locations)
+        new.keywords.set(self.keywords)
+        new.save()
+
+        return new
+
+    def add_contribution(
+        self,
+        institutional_contribution=None,
+        link_to_graphic=None,
+        link_to_data=None,
+        link=None,
+    ):
+        self.institutional_contribution = institutional_contribution
+        self.link_to_graphic = link_to_graphic
+        self.link_to_data = link_to_data
+        self.link = link
+
+    def add_raw_data(self, items):
+        logging.info(
+            f"Saving raw data {self.code} {self.object_name} {self.category} {self.context}"
+        )
+        self.save_raw_data(items)
+        logging.info(
+            f"Saved raw data {self.code} {self.object_name} {self.category} {self.context}"
+        )
+        self.record_status = choices.PUBLISHED
+        self.validity = choices.CURRENT
+        self.save()
+
+        if self.previous_record:
+            self.previous_record.posterior_record = self
+            self.previous_record.validity = choices.OUTDATED
+            self.previous_record.save()
+
+    def add_context(self, institutions=None, locations=None, thematic_areas=None):
+        if institutions or locations or thematic_areas:
+            self.save()
+            self.institutions.set(institutions)
+            self.locations.set(locations)
+            self.thematic_areas.set(thematic_areas)
+            self.save()
