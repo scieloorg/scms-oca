f5b58bcae07ab03f6050bba923088a2c6cab6a1a
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-06-12 15:28:40 -0300

    Cria nova tarefa para gerar os indicadores de diretórios

--- indicator/tasks.py
+++ indicator/tasks.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext as _
 from config import celery_app
 
 from indicator import controller
+from indicator import directory
 
 
 User = get_user_model()
@@ -56,3 +57,20 @@ def task_generate_evolution_of_scientific_production(
         context_params=context_params,
         context_id=context_id,
     )
+
+@celery_app.task(bind=True, name=_("Geração de indicadores de ações em Ciência Aberta"))
+def task_generate_directory_indicators(
+    self, creator_id, action_name=None, filter_by=None, group_by=None
+):
+    creator = User.objects.get(id=creator_id) or User.objects.first()
+    action__names = [action_name]
+
+    filter_params = [{}]
+    if filter_by == "thematic_area":
+        filter_params = directory.get_thematic_area_filter_params()
+    elif filter_by == "location":
+        filter_params = directory.get_location_filter_params()
+
+    group_by = group_by or {}
+
+    directory.generate_indicators(creator, action__names, filter_params, group_by)
