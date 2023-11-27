4737eac0bdd5945ec279cfe3d2d5dd21f9cb188b
GitInno <86991526+gitnnolabs@users.noreply.github.com>
2023-11-27 07:32:36 -0300

    Garante os IDs possa ser de diferentes modelos.

--- indicator/tasks.py
+++ indicator/tasks.py
@@ -100,10 +100,11 @@ def task_generate_article_indicators(self, user_id, indicators):
             )
 
             indicator_model.save()
+            items = []
 
-            ct = ContentType.objects.get(model=ind.model)
-            model = ct.model_class()
-
-            items = model.objects.filter(pk__in=[id for id in ind.ids.keys()])
+            for model in ind.models:
+                ct = ContentType.objects.get(model=model)
+                md = ct.model_class()
+                items += md.objects.filter(pk__in=[id for id in ind.ids.keys()])
 
             indicator_model.save_raw_data(items=items, ids=ind.get_ids())
