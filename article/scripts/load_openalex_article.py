24cf6a5f7470dab95176edd1ffa1f9b04aba9679
GitInno <86991526+gitnnolabs@users.noreply.github.com>
2023-11-29 11:05:54 -0300

    Ajusta a lista de param do script load_openalex_article

--- article/scripts/load_openalex_article.py
+++ article/scripts/load_openalex_article.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext as _
 from article.tasks import article_source_to_article
 
 
-def run(user_id, update):
+def run(user_id, update, year):
     """
     Load the article from SourceArticle model to Article model.
     """
@@ -11,7 +11,7 @@ def run(user_id, update):
         # https://openalex.org/I17974374
         # https://openalex.org/I16655442
         # https://openalex.org/I179964525
-        # article_source_to_article(int(user_id), size=100000)
-        article_source_to_article(int(user_id), intitution_id="https://openalex.org/I17974374")
+        article_source_to_article(int(user_id), year=str(year))
+        # article_source_to_article(int(user_id), intitution_id="https://openalex.org/I17974374")
     else:
         print(_("Param user_id is required."))
