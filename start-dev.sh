4024ec805c81e2149d517505f6ded237139d706d
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2023-12-01 00:32:27 -0300

    start-dev.sh

---
+++ start-dev.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Change this value to the local ethernet.
+ethernet=enp2s0
+
+# Linux IP.
+export IP=$(/sbin/ip -o -4 addr list $ethernet | awk '{print $4}' | cut -d/ -f1)
+
+# Mac OS IP.
+# export IP=$(ifconfig $ethernet | grep inet | grep -v inet6 | awk '{print $2}')
+
+export DATABASE_URL=postgres://GVRFlLmcCNfGLhsFvSnCioYOPJPYpyfj:BQ4hSUL4rdj5WZLdR8ilDLRQMvCtzo0caMaXDO0olGsmycQjlcZlTVK9DepZR8kk@$IP:5432/core
+# export DATABASE_URL=postgres://oca_hml:BQ4hSUL4rdj5WZLdR8ilDLRQMvCtzo0caMaXDO0olGsmycQjlcZlTVK9DepZR8kk@node01-ocabr.scielo.org/oca_hml
+export CELERY_BROKER_URL=redis://$IP:6379/0
+export USE_DOCKER=no
+export IPYTHONDIR=/app/.ipython
+export REDIS_URL=redis://$IP:6379/0
+export CELERY_FLOWER_USER=PhFRdLexbrsBvrrbSXxjcMMOcVOavCrZ
+export CELERY_FLOWER_PASSWORD=QgScyefPrYhHgO6onW61u0nazc5xdBuP4sM7jMRrBBFuA2RjsFhZLp7xbVYZbrwR
+export EMAIL_HOST=$IP
+# export SOLR_URL=http://$IP:8983/solr/asite/
+
+
+docker stop oca_local_django
+# workon scms
+python manage.py runserver_plus 0.0.0.0:8000 
