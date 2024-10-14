5748825fe259214bd114973e3d4a0a3421933d0e
GitInno <86991526+gitnnolabs@users.noreply.github.com>
2024-10-14 04:47:44 -0300

    Adiciona o modelo de journal no admin.

---
+++ journal/wagtail_hooks.py
@@ -0,0 +1,34 @@
+from django.utils.translation import gettext as _
+
+from wagtail.contrib.modeladmin.options import ModelAdmin
+
+from .models import SourceJournal
+
+
+class SourceJournalAdmin(ModelAdmin):
+    model = SourceJournal
+    menu_label = _(
+        "Source Journals"
+    )  # ditch this to use verbose_name_plural from model
+    menu_icon = "folder-open-inverse"  # change as required
+    add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
+    exclude_from_explorer = (
+        False  # or True to exclude pages of this type from Wagtail's explorer view
+    )
+
+    list_display = (
+        "issn",
+        "specific_id",
+        "updated",
+        "created",
+    )
+
+    list_filter = (
+        "issn",
+        "source",
+    )
+    search_fields = (
+        "title"
+        "issn",
+        "specific_id",
+    )
