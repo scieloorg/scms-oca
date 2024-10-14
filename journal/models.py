b1308049d6f0b001d556ff3f8939e08649af699a
GitInno <86991526+gitnnolabs@users.noreply.github.com>
2024-10-14 04:34:45 -0300

    Adiciona a aplicação journal com modelo inicial e migração.

---
+++ journal/models.py
@@ -0,0 +1,142 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+from core.models import Source
+
+
+class SourceJournal(models.Model):
+    specific_id = models.CharField(
+        _("Specific Id"), max_length=255, null=False, blank=False
+    )
+    issn = models.CharField(_("DOI"), max_length=255, null=True, blank=False)
+    title = models.CharField(_("Title"), max_length=1024, null=True, blank=False)
+    updated = models.CharField(
+        _("Source updated date"), max_length=50, null=True, blank=False
+    )
+    created = models.CharField(
+        _("Source created date"), max_length=50, null=True, blank=False
+    )
+    raw = models.JSONField(_("JSON File"), null=True, blank=True)
+    source = models.ForeignKey(
+        Source,
+        verbose_name=_("Source"),
+        null=True,
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        indexes = [
+            models.Index(
+                fields=[
+                    "issn",
+                ]
+            ),
+            models.Index(
+                fields=[
+                    "specific_id",
+                ]
+            ),
+            models.Index(
+                fields=[
+                    "title",
+                ]
+            ),
+        ]
+
+    def __unicode__(self):
+        return str("%s") % (self.issn or self.specific_id)
+
+    def __str__(self):
+        return str("%s") % (self.issn or self.specific_id)
+
+    @property
+    def has_specific_id(self):
+        return bool(self.specific_id)
+
+    @classmethod
+    def get(cls, **kwargs):
+        """
+        This function will try to get the source journal by attributes:
+
+            * issn
+            * specific_id
+
+        The kwargs must be a dict, something like this:
+
+            {
+                "specific_id": "https://openalex.org/sources/S183843087",
+                "issn": "1234-5678",
+                "source": OPENALEX,
+            }
+
+        return source journal|None
+
+        This function can raise:
+            ValueError
+            SourceJournal.DoesNotExist
+            SourceJournal.MultipleObjectsReturned
+        """
+
+        filters = {}
+
+        if (
+            not kwargs.get("issn")
+            and not kwargs.get("specific_id")
+            and not kwargs.get("source")
+        ):
+            raise ValueError("Param issn or specific_id is required")
+
+        if kwargs.get("specific_id"):
+            filters = {
+                "specific_id": kwargs.get("specific_id"),
+                "source": kwargs.get("source"),
+            }
+        elif kwargs.get("issn"):
+            filters = {"issn": kwargs.get("issn"), "source": kwargs.get("source")}
+
+        return cls.objects.get(**filters)
+
+
+    @classmethod
+    def create_or_update(cls, **kwargs):
+        """
+        This function will try to get the journal by issn.
+
+        If the journal exists update, otherwise create.
+
+        The kwargs must be a dict, something like this:
+
+            {
+                "issn": "1234-5678",
+                "title": "Update the record",
+                "number": "999",
+                "volume": "9",
+                "sources": list of <sources> [<source>, <source>]
+            }
+
+        return journal(object), 0|1
+
+        0 = updated
+        1 = created
+
+        """
+
+        try:
+            journal = cls.get(**kwargs)
+            created = 0
+        except SourceJournal.DoesNotExist:
+            journal = cls.objects.create()
+            created = 1
+        except SourceJournal.MultipleObjectsReturned as e:
+            print(_("The source journal table have duplicity...."))
+            raise (SourceJournal.MultipleObjectsReturned)
+
+        journal.issn = kwargs.get("issn")
+        journal.specific_id = kwargs.get("specific_id")
+        journal.updated = kwargs.get("updated")
+        journal.created = kwargs.get("created")
+        journal.raw = kwargs.get("raw")
+        journal.source = kwargs.get("source")
+        journal.save()
+
+        return journal, created
