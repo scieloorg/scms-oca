34b9a8b34d5840dfc750ac895f7301fa61754d73
GitInno <86991526+gitnnolabs@users.noreply.github.com>
2025-06-01 23:33:09 -0300

    Adiciona a capacidade de colocar conteúdo em iframe nas páginas freepage.

--- freepage/models.py
+++ freepage/models.py
@@ -1,12 +1,26 @@
-from wagtail.fields import RichTextField
+from django.db import models
 from wagtail.admin.panels import FieldPanel
-
+from wagtail.fields import RichTextField
 from wagtail.models import Page
 
 
 class FreePage(Page):
     body = RichTextField()
 
+    embed = models.TextField(
+        "Embed",
+        null=True,
+        blank=True,
+        help_text="Embed content for the page, such as an iframe or other embed code.",
+    )
+    use_only_embed = models.BooleanField(
+        default=False,
+        verbose_name="Set only embed",
+        help_text="If checked, the page will only display the embed content without the body text.",
+    )
+
     content_panels = Page.content_panels + [
         FieldPanel("body", classname="full"),
+        FieldPanel("embed", classname="full"),
+        FieldPanel("use_only_embed", classname="full"),
     ]
