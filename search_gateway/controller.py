f47dfb2c7d49fda0b3150d4fd987f61d9a57932c
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2025-12-02 11:16:40 -0300

    Add funções para search

--- search_gateway/controller.py
+++ search_gateway/controller.py
@@ -1,9 +1,78 @@
-from .client import get_es_client
-from . import data_sources
-from . import query as query_builder
+from . import data_sources, data_sources_with_settings
 from . import parser as response_parser
+from . import query as query_builder
+from .client import get_es_client
 
 FILTERS_CACHE = {}
+
+
+def get_mapped_filters(filters, field_settings):
+    """
+    Function to map the filters to the real field names.
+    Args:
+        filters: The filters to apply to the search.
+        field_settings: The field settings to use to map the filters.
+    Returns:
+        A dictionary containing the mapped filters.
+    """
+
+    if filters:
+        mapped_filters = {}
+        for key, value in filters.items():
+            if key in field_settings:
+                real_field_name = field_settings[key].get("index_field_name")
+                mapped_filters[real_field_name] = value
+        return mapped_filters
+    return None
+
+
+def search_documents(
+    data_source_name, query_text=None, filters=None, page=1, page_size=10, source_fields=None
+):
+    """
+    Function to search documents in the database.
+    Args:
+        data_source_name: The name of the data source to search in.
+        query_text: The text to search for.
+        filters: The filters to apply to the search.
+        page: The page number to return.
+        page_size: The number of results per page.
+        source_fields: The fields to return in the results.
+    Returns:
+        A dictionary containing the search results.
+    """
+    
+    es = get_es_client()
+    data_source = data_sources_with_settings.get_data_source(data_source_name)
+    field_settings = data_sources_with_settings.get_field_settings(data_source_name)
+    mapped_filters = get_mapped_filters(filters, field_settings)
+
+    body = query_builder.build_document_search_body(
+        query_text=query_text,
+        filters=mapped_filters,
+        page=page,
+        page_size=page_size,
+        source_fields=source_fields,
+    )
+    print(body)
+    res = es.search(index=data_source.get("index_name"), body=body)
+    parsed_results = response_parser.parse_document_search_response(res)
+    return parsed_results
+
+
+def search_as_you_type(data_source_name, query_text, field_name):
+    es = get_es_client()
+    data_source = data_sources_with_settings.get_data_source(data_source_name)
+    fl_name = data_sources_with_settings.get_index_field_name_from_data_source(data_source_name, field_name)
+    size = data_sources_with_settings.get_size_by_field_name(data_source_name, field_name)
+    body = query_builder.build_search_as_you_type_body(
+        field_name=fl_name,
+        query=query_text,
+        agg_size=size,
+    )
+    res = es.search(index=data_source.get("index_name"), body=body)
+    parsed_results = response_parser.parse_search_item_response(res)
+    return parsed_results
 
 
 def get_search_item_results(q, data_source_name, field_name):
@@ -39,7 +108,7 @@ def get_search_item_results(q, data_source_name, field_name):
         return None, "Error executing or parsing search"
 
 
-def get_filters_data(data_source_name):
+def get_filters_data(data_source_name, exclude_fields=None):
     """
     Orchestrates the retrieval of filter options.
     """
@@ -58,7 +127,7 @@ def get_filters_data(data_source_name):
     if not field_settings:
         return None, "Field settings not found"
 
-    aggs = query_builder.build_filters_aggs(field_settings)
+    aggs = query_builder.build_filters_aggs(field_settings, exclude_fields)
     body = {"size": 0, "aggs": aggs}
 
     try:
