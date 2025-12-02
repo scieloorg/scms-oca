2489c5965ff7ec860fb7765764c69585fd578120
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2025-12-02 11:16:53 -0300

    add parse_document_search_response e _transform_document_search_results

--- search_gateway/parser.py
+++ search_gateway/parser.py
@@ -1,9 +1,11 @@
+from django.utils.translation import gettext as _
+
 def parse_search_item_response(response):
     """
     Parses the response for a search-as-you-type query.
     """
     buckets = response.get("aggregations", {}).get("unique_items", {}).get("buckets", [])
-    return [{"id": b["key"], "text": b["key"]} for b in buckets]
+    return [{"key": b["key"], "doc_count": b["doc_count"]} for b in buckets]
 
 
 def parse_filters_response(response):
@@ -14,3 +16,26 @@ def parse_filters_response(response):
         k: [b["key"] for b in v["buckets"]]
         for k, v in response.get("aggregations", {}).items()
     }
+
+
+def parse_document_search_response(documents):
+    transformed_documents = _transform_document_search_results(documents)
+    data_documents = {
+        "search_results": transformed_documents,
+        "total_results": documents["hits"]["total"]["value"],
+    }
+    return data_documents
+
+
+def _transform_document_search_results(search_results):
+    transformed_hits = []
+    for hit in search_results["hits"]["hits"]:
+        transformed_hits.append(
+            {
+                "index": hit.get("_index"),
+                "id": hit.get("_id"),
+                "source": hit.get("_source", {}),
+                "score": hit.get("_score"),
+            }
+        )
+    return transformed_hits
