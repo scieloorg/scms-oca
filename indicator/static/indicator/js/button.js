ca75d8f7d687ad2bf0b0443f332c539e1ff13d72
Rafael JPD <rafael@pitangainnovare.com.br>
2026-01-08 10:59:34 -0300

    Encapsula listener de botão a listener de documento

--- indicator/static/indicator/js/button.js
+++ indicator/static/indicator/js/button.js
@@ -1,9 +1,11 @@
-document.querySelectorAll('.toggle-not').forEach(button => {
-    button.addEventListener('click', () => {
-        const group = button.closest('.input-group');
-        if (!group) return;
-        group.classList.toggle('not-active');
-        const status = button.getAttribute("aria-pressed") === "true";
-        button.setAttribute('aria-pressed', !status);
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.toggle-not').forEach(button => {
+        button.addEventListener('click', () => {
+            const group = button.closest('.input-group');
+            if (!group) return;
+            group.classList.toggle('not-active');
+            const status = button.getAttribute("aria-pressed") === "true";
+            button.setAttribute('aria-pressed', !status);
+        });
     });
 });
