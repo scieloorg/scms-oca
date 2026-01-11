a68d757fdc645d81833d8db0ee5c10ba68b9809c
Rafael JPD <rafael@pitangainnovare.com.br>
2026-01-10 21:17:52 -0300

    Cria função JS para facilitar criação de Select2 com search as you type

--- indicator/static/indicator/js/button.js
+++ indicator/static/indicator/js/button.js
@@ -65,3 +65,38 @@ function setupSelect2Multiple(fieldId, placeholder="", allow_clear=true, width="
         console.error('Error initializing Select2 for field', fieldId, '. Error: ', error)
     }
 }
+
+/* Função para facilitar construção de objeto Select2 com busca em índice */
+function setupSelect2SearchAsYouType(fieldId, data_source, placeholder="Start typing to search...", allow_clear=true) {
+    try {
+        const selectElement = document.getElementById(fieldId);
+        if (selectElement) {
+            $(selectElement).select2(
+                {
+                    ajax: {
+                        url: `/search-gateway/search-item/?field_name=${fieldId}&data_source=${data_source}`,
+                        dataType: 'json',
+                        delay: 300,
+                        data: params => ({q: params.term}),
+                        processResults: data => {
+                            return {
+                                results: data.results.map(item => ({
+                                    id: item.key,
+                                    text: item.key
+                                }))
+                            };
+                        }
+                    },
+                    minimumInputLength: 2,
+                    placeholder: placeholder,
+                    theme: 'bootstrap-5',
+                    allowClear: allow_clear,
+                }
+            ).on('select2:open', function (e) {
+                document.querySelector(`[aria-controls="select2-${e.target.id}-results"]`).focus();
+            });
+        }
+    } catch (error) {
+        console.error('Error initializing Select2 for field', fieldId, 'in data source', data_source, '. Error: ', error);
+    }
+}
