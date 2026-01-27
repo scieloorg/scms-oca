dffd859cab0fef048da35a6f0d2b18722e850a97
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2026-01-26 21:16:10 -0300

    Cria modeladmin group

--- harvest/wagtail_hooks.py
+++ harvest/wagtail_hooks.py
@@ -3,37 +3,42 @@ from wagtail.contrib.modeladmin.options import (
     ModelAdminGroup,
     modeladmin_register,
 )
-from .models import HarvestedPreprint, HarvestedSciELOData, HarvestedBooks
+
+from .models import HarvestedBooks, HarvestedPreprint, HarvestedSciELOData
 
 
 class HarvestedPreprintModelAdmin(ModelAdmin):
     model = HarvestedPreprint
-    menu_label = "Harvest Preprint"
+    menu_label = "Preprint"
     list_display = ("identifier", "harvest_status", "datestamp", "created")
     search_fields = ("identifier", "source_url")
     list_filter = ("harvest_status", "index_status")
 
 
-modeladmin_register(HarvestedPreprintModelAdmin)
-
-
 class HarvestedSciELODataModelAdmin(ModelAdmin):
     model = HarvestedSciELOData
-    menu_label = "Harvest SciELO Data"
+    menu_label = "SciELO Data"
     list_display = ("identifier", "harvest_status", "datestamp", "created")
     search_fields = ("identifier", "source_url")
     list_filter = ("harvest_status", "index_status", "type_data")
-
-
-modeladmin_register(HarvestedSciELODataModelAdmin)
 
 
 class HarvestedBooksModelAdmin(ModelAdmin):
     model = HarvestedBooks
-    menu_label = "Harvest Books"
+    menu_label = "Books"
     list_display = ("identifier", "harvest_status", "datestamp", "created")
     search_fields = ("identifier", "source_url")
     list_filter = ("harvest_status", "index_status", "type_data")
 
 
-modeladmin_register(HarvestedBooksModelAdmin)
+class HarvestAdminGroup(ModelAdminGroup):
+    menu_label = "Harvest"
+    menu_icon = "download"
+    items = (
+        HarvestedPreprintModelAdmin,
+        HarvestedSciELODataModelAdmin,
+        HarvestedBooksModelAdmin,
+    )
+
+
+modeladmin_register(HarvestAdminGroup)
