984977383a4f2a40359fd2d77e1e12a0205734e3
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2026-02-20 14:18:00 -0300

    Fix: Function call in signal ThematicArea

--- usefulmodels/signals.py
+++ usefulmodels/signals.py
@@ -2,13 +2,15 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from education_directory.models import EducationDirectory
+from education_directory.scripts.index_opensearch import index_education_instance
 from event_directory.models import EventDirectory
+from event_directory.scripts.index_opensearch import index_event_instance
 from infrastructure_directory.models import InfrastructureDirectory
-from infrastructure_directory.scripts.index_opensearch import index_infrastructure_instance
-from policy_directory.models import PolicyDirectory
-from policy_directory.scripts.index_opensearch import (
-    index_policy_instance,
+from infrastructure_directory.scripts.index_opensearch import (
+    index_infrastructure_instance,
 )
+from policy_directory.models import PolicyDirectory
+from policy_directory.scripts.index_opensearch import index_policy_instance
 
 from .models import ThematicArea
 
@@ -21,12 +23,12 @@ def sync_related_directories_on_thematic_area_save(sender, instance, created, **
         index_policy_instance(instance=policy)
 
     for event in EventDirectory.objects.filter(thematic_areas=instance).iterator():
-        index_policy_instance(instance=event)
+        index_event_instance(instance=event)
 
     for education in EducationDirectory.objects.filter(
         thematic_areas=instance
     ).iterator():
-        index_policy_instance(instance=education)
+        index_education_instance(instance=education)
 
     for infrastructure in InfrastructureDirectory.objects.filter(
         thematic_areas=instance
