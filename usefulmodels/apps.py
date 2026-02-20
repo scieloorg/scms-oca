1e85d261a487ed68be897b00647146da8856bddd
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2026-02-20 12:27:30 -0300

    Cria signal para atualizar o indice ao mudar ThematicArea

--- usefulmodels/apps.py
+++ usefulmodels/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UsefulmodelsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "usefulmodels"
+
+    def ready(self):
+        from . import signals  # noqa: F401
