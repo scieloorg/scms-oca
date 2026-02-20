9c47d9ef461c4662c8ea876fe63c546fb5083bbd
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2026-02-20 12:29:41 -0300

    remove imports

--- usefulmodels/signals.py
+++ usefulmodels/signals.py
@@ -2,20 +2,11 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from education_directory.models import EducationDirectory
-from education_directory.scripts.index_opensearch import (
-    delete_education_instance,
-    index_education_instance,
-)
 from event_directory.models import EventDirectory
-from event_directory.scripts.index_opensearch import (
-    delete_event_instance,
-    index_event_instance,
-)
 from infrastructure_directory.models import InfrastructureDirectory
 from infrastructure_directory.scripts.index_opensearch import index_infrastructure_instance
 from policy_directory.models import PolicyDirectory
 from policy_directory.scripts.index_opensearch import (
-    delete_policy_instance,
     index_policy_instance,
 )
 
