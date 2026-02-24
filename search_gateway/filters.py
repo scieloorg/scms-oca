953e89be329f8e0eccb2e8cdc8dfc433e5be48cf
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2026-02-24 12:00:18 -0300

    Adiciona categorias filtros

--- search_gateway/filters.py
+++ search_gateway/filters.py
@@ -1,20 +1,35 @@
 from django.utils.translation import gettext as _
 
 FILTER_CATEGORIES = {
-    "source": {
-        "label": _("Source"),
-        "icon": "icon-book",
+    "scope": {
+        "label": _("Data scope"),
+        "icon": "icon-filter",
         "order": 1,
+    },
+    "indexing": {
+        "label": _("Indexing"),
+        "icon": "icon-filter",
+        "order": 2,
+    },
+    "source": {
+        "label": _("Source Identity"),
+        "icon": "icon-book",
+        "order": 3,
     },
     "document": {
         "label": _("Document"),
         "icon": "icon-file-text",
-        "order": 2,
+        "order": 4,
     },
+    "category": {
+        "label": _("Category"),
+        "icon": "icon-file-text",
+        "order": 5,
+    },    
     "author_affiliation": {
         "label": _("Author Affiliation"),
         "icon": "icon-barcode",
-        "order": 3,
+        "order": 6,
     },
     "other": {
         "label": _("Other Filters"),
