8dc696df9bf6a2a9198ca0838abc660c5b5a96b1
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2026-03-17 10:42:53 -0300

    Add funções de directory_import.py

--- education_directory/views.py
+++ education_directory/views.py
@@ -1,23 +1,32 @@
 import csv
 import os
-from datetime import datetime
 
-from django.http import Http404, HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, redirect
-from django.utils.translation import gettext as _
-from django.template.loader import render_to_string
 from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect
+from django.template.loader import render_to_string
+from django.utils.translation import gettext as _
 from wagtail.admin import messages
 from wagtail_modeladmin.views import CreateView, EditView
 
 from core import tasks
-from core.libs import chkcsv
+from core.directory_import import (
+    build_institutions,
+    build_thematic_areas,
+    download_sample_file,
+    get_dates_and_times,
+    get_directory_instance,
+    get_row_value,
+    set_common_fields,
+    sync_keywords,
+    sync_related_items,
+    validate_directory_file,
+)
 from core_settings.models import Moderation
-from institution.models import Institution
-from usefulmodels.models import Action, Practice, ThematicArea
-from .permission_helper import EducationDirectoryPermissionHelper
+from usefulmodels.models import Action
 
 from .models import EducationDirectory, EducationDirectoryFile
+from .permission_helper import EducationDirectoryPermissionHelper
 
 
 class EducationDirectoryCreateView(CreateView):
@@ -134,44 +143,12 @@ class EducationDirectoryFileCreateView(CreateView):
 
 def validate(request):
     """
-    This view function validade a csv file based on a pre definition os the fmt
-    file.
-
-    The check_csv_file function check that all of the required columns and data
-    are present in the CSV file, and that the data conform to the appropriate
-    type and other specifications, when it is not valid return a list with the
-    errors.
+    Validate CSV file for EducationDirectory import.
     """
-    errorlist = []
-    file_id = request.GET.get("file_id", None)
-
-    if file_id:
-        file_upload = get_object_or_404(EducationDirectoryFile, pk=file_id)
-
-    if request.method == "GET":
-        try:
-            upload_path = file_upload.attachment.file.path
-            cols = chkcsv.read_format_specs(
-                os.path.dirname(os.path.abspath(__file__)) + "/chkcsvfmt.fmt",
-                True,
-                False,
-            )
-            errorlist = chkcsv.check_csv_file(
-                upload_path, cols, True, True, True, False
-            )
-            if errorlist:
-                raise Exception(_("Vlidation error"))
-            else:
-                file_upload.is_valid = True
-                fp = open(upload_path)
-                file_upload.line_count = len(fp.readlines())
-                file_upload.save()
-        except Exception as ex:
-            messages.error(request, _("Vlidation error: %s") % errorlist)
-        else:
-            messages.success(request, _("File successfully validated!"))
-
-    return redirect(request.META.get("HTTP_REFERER"))
+    format_file_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "chkcsvfmt.fmt"
+    )
+    return validate_directory_file(request, EducationDirectoryFile, format_file_path)
 
 
 def import_file(request):
@@ -197,95 +174,31 @@ def import_file(request):
             data = csv.DictReader(
                 csvfile, delimiter=settings.DIRECTORY_IMPORT_DELIMITER
             )
-
             for line, row in enumerate(data):
+                record_id = get_row_value(row, "Id")
+                ed = get_directory_instance(EducationDirectory, record_id)
 
-                record_id = row.get("Id", "").strip()
-                if (
-                    record_id
-                    and EducationDirectory.objects.filter(id=record_id).exists()
-                ):
-                    ed = EducationDirectory.objects.get(id=record_id)
-                else:
-                    ed = EducationDirectory()
+                set_common_fields(
+                    ed, row, request.user, action_filter="educação / capacitação"
+                )
+                get_dates_and_times(ed, row)
 
-                ed.title = row["Title"]
-                ed.link = row["Link"]
-                ed.description = row["Description"]
-                if row["Start Date"]:
-                    ed.start_date = datetime.strptime(row["Start Date"], "%d/%m/%Y")
-                if row["End Date"]:
-                    ed.end_date = datetime.strptime(row["End Date"], "%d/%m/%Y")
-                if row["Start Time"]:
-                    ed.start_time = row["Start Time"]
-                if row["End Time"]:
-                    ed.end_time = row["End Time"]
-                ed.creator = request.user
                 ed.save()
-
-                # Institution
-                inst_name = row["Institution Name"].strip()
-                if inst_name:
-                    inst_country = row["Institution Country"].strip()
-                    inst_state = row["Institution State"].strip()
-                    inst_city = row["Institution City"].strip()
-
-                    institution = Institution.get_or_create(
-                        inst_name, inst_country, inst_state, inst_city, request.user
-                    )
-                    ed.institutions.add(institution)
-
-                # Thematic Area
-                level0 = row["Thematic Area Level0"].strip()
-                if level0:
-                    level1 = row["Thematic Area Level1"].strip()
-                    level2 = row["Thematic Area Level2"].strip()
-                    the_area = ThematicArea.get_or_create(
-                        level0, level1, level2, request.user
-                    )
-
-                    ed.thematic_areas.add(the_area)
-
-                # Keywords
-                if row["Keywords"]:
-                    for key in row["Keywords"].split("|"):
-                        ed.keywords.add(key)
-
-                if row["Classification"]:
-                    ed.classification = row["Classification"]
-
-                # Practice
-                if row["Practice"]:
-                    practice_name = row["Practice"]
-                    if Practice.objects.filter(name=practice_name).exists():
-                        practice = Practice.objects.get(name=practice_name)
-                        ed.practice = practice
-                    else:
-                        messages.error(
-                            request, _("Unknown Practice, line: %s") % str(line + 2)
-                        )
-
-                # Action = educação / capacitação"
-                if row["Action"]:
-                    if Action.objects.filter(
-                        name__icontains="educação / capacitação"
-                    ).exists():
-                        ed.action = Action.objects.get(
-                            name__icontains="educação / capacitação"
-                        )
-
-                if row["Source"]:
-                    ed.source = row["Source"]
+                sync_related_items(
+                    ed.institutions,
+                    build_institutions(row, request.user),
+                )
+                sync_related_items(
+                    ed.thematic_areas,
+                    build_thematic_areas(row, request.user),
+                )
+                sync_keywords(ed.keywords, row.get("Keywords"))
 
                 ed.save()
     except Exception as ex:
         messages.error(
             request,
-            _("Import error: %(error)s, Line: %(line)s")
-            % {
-                "error": ex,
-                "line": line + 2,
-            },
+            _(f"Import error: {ex}, Line: {line + 2}")
         )
     else:
         messages.success(request, _("File imported successfully!"))
@@ -295,14 +208,9 @@ def import_file(request):
 
 def download_sample(request):
     """
-    This view function a CSV sample for model EducationDirectoryFile.
+    Download CSV sample file for EducationDirectory import.
     """
-    file_path = os.path.dirname(os.path.abspath(__file__)) + "/example_education.csv"
-    if os.path.exists(file_path):
-        with open(file_path, "rb") as fh:
-            response = HttpResponse(fh.read(), content_type="text/csv")
-            response["Content-Disposition"] = "inline; filename=" + os.path.basename(
-                file_path
-            )
-            return response
-    raise Http404
+    sample_file_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "example_education.csv"
+    )
+    return download_sample_file(sample_file_path)
