4c1e4c023e7c37433e5844d13767863546f77e35
Rafael JPD <rafael@pitangainnovare.com.br>
2026-03-23 21:35:12 -0300

    Simplifica parser e o renomeia

--- search_gateway/response_parser.py
+++ search_gateway/response_parser.py
@@ -1,11 +1,15 @@
-from . import transforms
+from .transforms import apply_display_transform_from_field_settings
+
+
+def _get_agg_buckets(response, agg_name):
+    return response.get("aggregations", {}).get(agg_name, {}).get("buckets", [])
 
 
 def _transform_bucket(field_settings, field_name, bucket):
     value = bucket.get("key")
     return {
         "key": value,
-        "label": transforms.apply_display_transform_from_field_settings(
+        "label": apply_display_transform_from_field_settings(
             field_settings,
             field_name,
             value,
@@ -14,36 +18,28 @@ def _transform_bucket(field_settings, field_name, bucket):
     }
 
 
-def parse_search_item_response_with_transform(response, data_source, field_name):
+def parse_search_item_response(response, data_source, field_name):
     field_settings = data_source.get_field_settings_dict()
-    buckets = response.get("aggregations", {}).get("unique_items", {}).get("buckets", [])
+    buckets = _get_agg_buckets(response, "unique_items")
     return [_transform_bucket(field_settings, field_name, bucket) for bucket in buckets]
 
 
-def parse_filters_response_with_transform(response, data_source):
+def parse_filters_response(response, data_source):
     field_settings = data_source.get_field_settings_dict()
     return {
         agg_name: [
             _transform_bucket(field_settings, agg_name, bucket)
-            for bucket in agg_data.get("buckets", [])
+            for bucket in _get_agg_buckets(response, agg_name)
         ]
-        for agg_name, agg_data in response.get("aggregations", {}).items()
+        for agg_name in response.get("aggregations", {})
     }
-
-
-def parse_search_item_response(response, data_source, field_name):
-    return parse_search_item_response_with_transform(response, data_source, field_name)
 
 
 def parse_terms_agg_keys(response, agg_name):
     if not response or not agg_name:
         return []
-    buckets = response.get("aggregations", {}).get(agg_name, {}).get("buckets", [])
+    buckets = _get_agg_buckets(response, agg_name)
     return [bucket.get("key") for bucket in buckets if bucket.get("key")]
-
-
-def parse_filters_response(response, data_source):
-    return parse_filters_response_with_transform(response, data_source)
 
 
 def parse_document_search_response(documents):
