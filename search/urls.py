cf4b066cf8b743f7ddd33ee55672a844b5e7cdf7
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2026-04-08 14:48:55 -0300

    add urls

--- search/urls.py
+++ search/urls.py
@@ -6,6 +6,8 @@ from .views import search_view_list
 app_name = "search"
 urlpatterns = [
     path("api/search-results-list/", search_view_list, name="search_results_list"),
-    path("api/citation-formats/", citation_views.citation_formats_view, name="citation_formats"),
+    path("api/citation-styles/", citation_views.citation_csl_styles_view, name="citation_csl_styles"),
+    path("api/citation-preview/", citation_views.citation_preview_view, name="citation_preview"),
+    path("api/citation-custom-style/", citation_views.citation_custom_style_view, name="citation_custom_style"),
     path("api/citation-export/", citation_views.citation_export_view, name="citation_export"),
 ]
