a1d2d75fd40983f8870623bb75ce993b8129149f
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2026-04-24 10:36:43 -0300

    Cria diretorio especifico para citation

--- search/citation/views.py
+++ search/citation/views.py
@@ -2,20 +2,24 @@ import functools
 import logging
 
 from django.http import HttpResponse, JsonResponse
+from django.utils import timezone
 from django.utils.translation import gettext as _
 from django.views.decorators.http import require_GET, require_POST
 
-from .citation_constants import CITATION_PRESET_STYLES
-from .citation_export_service import (
-    CitationBadRequest,
+from .render import list_installed_csl_styles
+from .constants import CITATION_PRESET_STYLES
+from .export_service import (
     build_citation_file,
     build_citation_preview,
     build_custom_citation,
+)
+from ..export_service import (
+    BadRequestError,
+    build_csv_file,
     extract_export_inputs,
     extract_preview_inputs,
     parse_request_body,
 )
-from .citeproc_render import list_installed_csl_styles
 
 logger = logging.getLogger(__name__)
 
@@ -26,7 +30,8 @@ def _json_error(message, *, status=400, **extra):
 
 def _file_attachment(content, mime, ext):
     resp = HttpResponse(content.encode("utf-8"), content_type=f"{mime}; charset=utf-8")
-    resp["Content-Disposition"] = f'attachment; filename="citation.{ext}"'
+    timestamp = timezone.localtime().strftime("%Y%m%d-%H%M")
+    resp["Content-Disposition"] = f'attachment; filename="citation-{timestamp}.{ext}"'
     return resp
 
 
@@ -35,7 +40,7 @@ def _handle_citation_errors(view_fn):
     def wrapper(request, *args, **kwargs):
         try:
             return view_fn(request, *args, **kwargs)
-        except CitationBadRequest as err:
+        except BadRequestError as err:
             return _json_error(err.message, status=err.status, **err.extra)
         except FileNotFoundError:
             logger.exception("CSL style not found")
@@ -62,7 +67,10 @@ def citation_csl_styles_view(request):
 def citation_export_view(request):
     body = parse_request_body(request.body)
     inputs = extract_export_inputs(body, request)
-    content, mime, ext = build_citation_file(inputs)
+    if inputs.format_key == "csv":
+        content, mime, ext = build_csv_file(inputs)
+    else:
+        content, mime, ext = build_citation_file(inputs)
     return _file_attachment(content, mime, ext)
 
 
@@ -80,6 +88,6 @@ def citation_custom_style_view(request):
     body = parse_request_body(request.body)
     style = body.get("style", "").strip().lower()
     if not style:
-        raise CitationBadRequest(_("Style name is required."))
+        raise BadRequestError(_("Style name is required."))
     inputs = extract_preview_inputs(body, request)
     return JsonResponse(build_custom_citation(inputs, style))
