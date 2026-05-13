a53e7c2d52ce7c43a3e0a170a6abf28948827920
Rafael JPD (Pitanga Innovare) <rafael@pitangainnovare.com.br>
2026-05-13 00:37:15 -0300

    adiciona novos testes

---
+++ etl/tests/presentation/test_wagtail_hooks.py
@@ -0,0 +1,36 @@
+from django.test import RequestFactory, SimpleTestCase
+
+from etl import wagtail_hooks
+
+
+class EtlAdminActionTests(SimpleTestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_trigger_pipeline_requires_post(self):
+        response = wagtail_hooks.trigger_pipeline_view(
+            self.factory.get("/admin/etl/trigger-pipeline/")
+        )
+
+        self.assertEqual(response.status_code, 405)
+
+    def test_trigger_pending_requires_post(self):
+        response = wagtail_hooks.trigger_pending_view(
+            self.factory.get("/admin/etl/trigger-pending/")
+        )
+
+        self.assertEqual(response.status_code, 405)
+
+    def test_reset_to_pending_requires_post(self):
+        response = wagtail_hooks.reset_to_pending_view(
+            self.factory.get("/admin/etl/reset-pending/")
+        )
+
+        self.assertEqual(response.status_code, 405)
+
+    def test_retry_failed_requires_post(self):
+        response = wagtail_hooks.retry_failed_view(
+            self.factory.get("/admin/etl/retry-failed/")
+        )
+
+        self.assertEqual(response.status_code, 405)
