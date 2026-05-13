e777fdd821bfa75cd5b497d141076c96020f1b7c
Rafael JPD <rafael@pitangainnovare.com.br>
2026-05-13 00:22:47 -0300

    cria values para acomodar valores de fallback e default

---
+++ indicator_journal/search/values.py
@@ -0,0 +1,19 @@
+GLOBAL_METRICS_BOOL_FIELDS = {
+    "is_scielo": "is_scielo_global",
+    "is_scopus": "is_scopus_global",
+    "is_wos": "is_wos_global",
+    "is_doaj": "is_doaj_global",
+    "is_journal_oa": "is_journal_oa_global",
+    "is_journal_multilingual": "is_journal_multilingual_global",
+}
+
+GLOBAL_METRICS_TEXT_FIELDS = {
+    "country": "journal_country",
+    "collection": "scielo_collection",
+    "publisher_name": "journal_publisher",
+    "journal_title": "journal_title",
+}
+
+DEFAULT_GLOBAL_METRIC = "global_field_normalized_impact"
+DEFAULT_GLOBAL_YEAR = "2020"
+DEFAULT_GLOBAL_LIMIT = 100
