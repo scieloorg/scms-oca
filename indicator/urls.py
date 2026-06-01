1cbd24231118966cc631e194ca7f4c68dffa9abc
Rafael JPD <rafael@pitangainnovare.com.br>
2026-05-31 22:10:32 -0300

    Atualiza urls de indicator mantendo apenas a que retorna dados de charts

--- indicator/urls.py
+++ indicator/urls.py
@@ -4,10 +4,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.indicator_view, {'data_source_name': 'scientific_production'}, name='indicator_home'),
-    path('data/', views.data_view, name='data'),
-    path('journal-metrics/', views.journal_metrics_view, name='indicator_journal_metrics'),
-    path('journal-metrics/timeseries/', views.journal_metrics_timeseries_view, name='indicator_journal_metrics_timeseries'),
-    path('periodical/timeseries/', views.periodical_timeseries_view, name='indicator_periodical_timeseries'),
-    path('<str:data_source_name>/', views.indicator_view, name='indicator'),
+    path('api/v1/chart-data/', views.chart_data_view, name='chart_data_api'),
 ]
